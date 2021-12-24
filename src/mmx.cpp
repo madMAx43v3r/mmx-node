d9610ab4915b13bb88b60166384c06329f852761
Max Wittal <max.wittal@mwittal.de>
2021-12-24 11:40:16 +0100

    more tools

--- src/mmx.cpp
+++ src/mmx.cpp
@@ -41,7 +41,6 @@ int main(int argc, char** argv)
 
 	std::string module;
 	std::string command;
-	std::string command2;
 	std::string node_url = ".mmx_node.sock";
 	std::string file_name;
 	std::string target_addr;
@@ -50,7 +49,6 @@ int main(int argc, char** argv)
 	double amount = 0;
 	vnx::read_config("$1", module);
 	vnx::read_config("$2", command);
-	vnx::read_config("$3", command2);
 	vnx::read_config("node", node_url);
 	vnx::read_config("file", file_name);
 	vnx::read_config("index", index);
@@ -86,142 +84,208 @@ int main(int argc, char** argv)
 		module.start_detached();
 	}
 
-	if(module == "wallet")
-	{
-		mmx::WalletClient wallet("Wallet");
-		try {
-			wallet.open_wallet(index);
-		}
-		catch(std::exception& ex) {
-			vnx::log_error() << "Failed to open wallet: " << ex.what();
-			goto failed;
-		}
-		if(command == "show")
+	try {
+		if(module == "wallet")
 		{
-			std::string currency = "MMX";
-			if(contract != mmx::addr_t()) {
-				currency = "?";
-			}
+			mmx::WalletClient wallet("Wallet");
 			try {
-				const auto amount = wallet.get_balance(contract);
-				std::cout << "Balance: " << amount / 1e6 << " " << currency << " (" << amount << ")" << std::endl;
-				std::cout << "Address[0]: " << wallet.get_address(0) << std::endl;
+				wallet.open_wallet(index);
 			}
 			catch(std::exception& ex) {
-				vnx::log_error() << "Failed with: " << ex.what();
+				vnx::log_error() << "Failed to open wallet: " << ex.what();
 				goto failed;
 			}
-		}
-		else if(command == "keys")
-		{
-			try {
+			if(command == "show")
+			{
+				int num_addrs = 1;
+				vnx::read_config("$3", num_addrs);
+
+				const auto amount = wallet.get_balance(contract);
+				std::cout << "Balance: " << amount / pow(10, params->decimals) << " MMX (" << amount << ")" << std::endl;
+				for(int i = 0; i < num_addrs; ++i) {
+					std::cout << "Address[" << i << "]: " << wallet.get_address(i) << std::endl;
+				}
+			}
+			else if(command == "keys")
+			{
 				const auto keys = wallet.get_farmer_keys(index);
 				std::cout << "Pool Public Key:   " << keys->pool_public_key << std::endl;
 				std::cout << "Farmer Public Key: " << keys->farmer_public_key << std::endl;
 			}
-			catch(std::exception& ex) {
-				vnx::log_error() << "Failed with: " << ex.what();
-				goto failed;
-			}
-		}
-		else if(command == "get")
-		{
-			if(command2 == "address")
+			else if(command == "get")
 			{
-				vnx::read_config("$4", index);
-				if(index < 0) {
-					vnx::log_error() << "Invalid index: " << index;
-					goto failed;
-				}
-				try {
+				std::string subject;
+				vnx::read_config("$3", subject);
+
+				if(subject == "address")
+				{
+					vnx::read_config("$4", index);
+					if(index < 0) {
+						vnx::log_error() << "Invalid index: " << index;
+						goto failed;
+					}
 					const auto addr = wallet.get_address(index);
 					std::cout << addr << std::endl;
 				}
-				catch(std::exception& ex) {
-					vnx::log_error() << "Failed to get address: " << ex.what();
-					goto failed;
+				else if(subject == "amount")
+				{
+					std::cout << wallet.get_balance(contract) << std::endl;
+				}
+				else if(subject == "balance")
+				{
+					std::cout << wallet.get_balance(contract) / pow(10, params->decimals) << std::endl;
+				}
+				else {
+					std::cerr << "Help: mmx wallet get [address | amount | balance]" << std::endl;
 				}
 			}
-			else {
-				std::cerr << "Help: mmx wallet get [address]" << std::endl;
-			}
-		}
-		else if(command == "send")
-		{
-			const uint64_t mojo = amount * pow(10, params->decimals);
-			if(mojo <= 0) {
-				vnx::log_error() << "Invalid amount: " << amount;
-				goto failed;
-			}
-			if(target == mmx::addr_t()) {
-				vnx::log_error() << "Missing destination address argument: -t | --target";
-				goto failed;
-			}
-			std::string currency = "MMX";
-			if(contract != mmx::addr_t()) {
-				currency = "?";
-			}
-			try {
-				const auto txid = wallet.send(mojo, target, contract);
-				std::cout << "Sent " << mojo / pow(10, params->decimals) << " " << currency << " to " << target << std::endl;
-				std::cout << "Transaction ID: " << txid << std::endl;
-			}
-			catch(std::exception& ex) {
-				vnx::log_error() << "Failed to send: " << ex.what();
-				goto failed;
-			}
-		}
-		else if(command == "create")
-		{
-			if(file_name.empty()) {
-				file_name = "wallet.dat";
-			}
-			if(vnx::File(file_name).exists()) {
-				vnx::log_error() << "Wallet file '" << file_name << "' already exists";
-				goto failed;
-			}
-			std::vector<uint8_t> seed(4096);
-			randombytes_buf(seed.data(), seed.size());
-
-			mmx::KeyFile wallet;
-			wallet.seed_value = mmx::hash_t(seed);
-
-			vnx::write_to_file(file_name, wallet);
-
-			std::cout << "Created wallet '" << file_name << "' with seed: "
-					<< std::endl << wallet.seed_value << std::endl;
-		}
-		else {
-			std::cerr << "Help: mmx wallet [show | send | create]" << std::endl;
-		}
-	}
-	else if(module == "node")
-	{
-		mmx::NodeClient node("Node");
-
-		if(command == "balance")
-		{
-			try {
-				mmx::addr_t address;
-				mmx::addr_t contract;
-				vnx::read_config("$3", address);
-				vnx::read_config("$4", contract);
-
+			else if(command == "send")
+			{
+				const uint64_t mojo = amount * pow(10, params->decimals);
+				if(mojo <= 0) {
+					vnx::log_error() << "Invalid amount: " << amount;
+					goto failed;
+				}
+				if(target == mmx::addr_t()) {
+					vnx::log_error() << "Missing destination address argument: -t | --target";
+					goto failed;
+				}
 				std::string currency = "MMX";
 				if(contract != mmx::addr_t()) {
 					currency = "?";
 				}
-				const auto amount = node.get_balance(address, contract);
-				std::cout << "Balance: " << amount / 1e6 << " " << currency << " (" << amount << ")" << std::endl;
+				const auto txid = wallet.send(mojo, target, contract);
+				std::cout << "Sent " << mojo / pow(10, params->decimals) << " " << currency << " to " << target << std::endl;
+				std::cout << "Transaction ID: " << txid << std::endl;
 			}
-			catch(std::exception& ex) {
-				vnx::log_error() << ex.what();
-				goto failed;
+			else if(command == "create")
+			{
+				if(file_name.empty()) {
+					file_name = "wallet.dat";
+				}
+				if(vnx::File(file_name).exists()) {
+					vnx::log_error() << "Wallet file '" << file_name << "' already exists";
+					goto failed;
+				}
+				std::vector<uint8_t> seed(4096);
+				randombytes_buf(seed.data(), seed.size());
+
+				mmx::KeyFile wallet;
+				wallet.seed_value = mmx::hash_t(seed);
+
+				vnx::write_to_file(file_name, wallet);
+
+				std::cout << "Created wallet '" << file_name << "' with seed: "
+						<< std::endl << wallet.seed_value << std::endl;
+			}
+			else {
+				std::cerr << "Help: mmx wallet [show | send | create]" << std::endl;
 			}
 		}
+		else if(module == "node")
+		{
+			mmx::NodeClient node("Node");
+
+			if(command == "balance")
+			{
+				mmx::addr_t address;
+				vnx::read_config("$3", address);
+
+				const auto amount = node.get_balance(address, contract);
+				std::cout << "Balance: " << amount / pow(10, params->decimals) << " MMX (" << amount << ")" << std::endl;
+			}
+			else if(command == "tx")
+			{
+				mmx::hash_t txid;
+				vnx::read_config("$3", txid);
+
+				const auto tx = node.get_transaction(txid);
+				if(!tx) {
+					vnx::log_error() << "No such transaction: " << txid;
+					goto failed;
+				}
+				const auto info = node.get_tx_key(txid);
+				const auto height = node.get_height();
+				if(info) {
+					std::cout << "Confirmations: " << height - info->height + 1 << std::endl;
+				} else {
+					std::cout << "Confirmations: none yet" << std::endl;
+				}
+				size_t i = 0;
+				for(const auto& out : tx->outputs) {
+					std::cout << "Output[" << i++ << "]: " << out.amount / pow(10, params->decimals) << " MMX (" << out.amount << ") -> " << out.address << std::endl;
+				}
+			}
+			else if(command == "get")
+			{
+				std::string subject;
+				vnx::read_config("$3", subject);
+
+				if(subject == "balance")
+				{
+					mmx::addr_t address;
+					vnx::read_config("$4", address);
+
+					const auto amount = node.get_balance(address, contract);
+					std::cout << amount / pow(10, params->decimals) << std::endl;
+				}
+				else if(subject == "amount")
+				{
+					mmx::addr_t address;
+					vnx::read_config("$4", address);
+
+					const auto amount = node.get_balance(address, contract);
+					std::cout << amount << std::endl;
+				}
+				else if(subject == "block" || subject == "header")
+				{
+					int64_t height = 0;
+					vnx::read_config("$4", height);
+
+					if(height < 0) {
+						vnx::log_error() << "Invalid height: " << height;
+						goto failed;
+					}
+					const auto block = node.get_block_at(height);
+					{
+						std::stringstream ss;
+						vnx::PrettyPrinter printer(ss);
+						if(subject == "header") {
+							vnx::accept(printer, block ? block->get_header() : nullptr);
+						} else {
+							vnx::accept(printer, block);
+						}
+						std::cout << ss.str() << std::endl;
+					}
+				}
+				else if(subject == "tx")
+				{
+					mmx::hash_t txid;
+					vnx::read_config("$4", txid);
+
+					const auto tx = node.get_transaction(txid);
+					{
+						std::stringstream ss;
+						vnx::PrettyPrinter printer(ss);
+						vnx::accept(printer, tx);
+						std::cout << ss.str() << std::endl;
+					}
+				}
+				else {
+					std::cerr << "Help: mmx node get [tx | balance | amount | block | header]" << std::endl;
+				}
+			}
+			else {
+				std::cerr << "Help: mmx node [tx | get | balance]" << std::endl;
+			}
+		}
+		else {
+			std::cerr << "Help: mmx [node | wallet | farm]" << std::endl;
+		}
 	}
-	else {
-		std::cerr << "Help: mmx [node | wallet | farm]" << std::endl;
+	catch(std::exception& ex) {
+		vnx::log_error() << "Failed with: " << ex.what();
+		goto failed;
 	}
 
 	goto exit;
