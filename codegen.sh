5a8b510fde55aa4fec68e7bc4e8f08c79386c00c
Max Wittal <max.wittal@mwittal.de>
2022-01-03 18:47:48 +0100

    codegen fix

--- codegen.sh
+++ codegen.sh
@@ -2,11 +2,10 @@
 
 cd $(dirname "$0")
 
-VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
-
+./vnx-base/codegen.sh
 ./vnx-addons/codegen.sh
 
-vnxcppcodegen --cleanup generated/ mmx interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/ vnx-addons/interface/ vnx-addons/modules/ interface/wallet/
-vnxcppcodegen --cleanup generated/contract/ mmx.contract interface/contract/ interface/ ${VNX_INTERFACE_DIR}/vnx/
-vnxcppcodegen --cleanup generated/operation/ mmx.operation interface/operation/ interface/ ${VNX_INTERFACE_DIR}/vnx/
-vnxcppcodegen --cleanup generated/solution/ mmx.solution interface/solution/ interface/ ${VNX_INTERFACE_DIR}/vnx/
+vnxcppcodegen --cleanup generated/ mmx interface/ modules/ vnx-base/interface/ vnx-addons/interface/ vnx-addons/modules/ interface/wallet/
+vnxcppcodegen --cleanup generated/contract/ mmx.contract interface/contract/ interface/ vnx-base/interface/
+vnxcppcodegen --cleanup generated/operation/ mmx.operation interface/operation/ interface/ vnx-base/interface/
+vnxcppcodegen --cleanup generated/solution/ mmx.solution interface/solution/ interface/ vnx-base/interface/
