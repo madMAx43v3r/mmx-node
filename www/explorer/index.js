c5e3063591a5fc4516a505850db0b55ca746d64d
Max Wittal <max.wittal@mwittal.de>
2022-01-10 00:06:03 +0100

    basic block explorer

--- www/explorer/index.js
+++ www/explorer/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const axios = require('axios')
+const {bech32, bech32m} = require('bech32')
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
@@ -10,7 +11,9 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('data'));
 
-function to_hex_str(a)
+const MMX_ADDR = "mmx1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqdgytev";
+
+function to_hex(a)
 {
 	var b = a.map(function (x) {
 		x = x.toString(16); // to hex
@@ -20,10 +23,50 @@ function to_hex_str(a)
 	return b;
 }
 
+function to_addr(a)
+{
+	return bech32m.encode('mmx', bech32m.toWords(a.reverse()));
+}
+
 function on_error(res, ex)
 {
 	console.log(ex.message);
 	res.status(500).send(ex.message);
+}
+
+function parse_block(data)
+{
+	let block = data;
+	block.hash = to_hex(block.hash);
+	block.prev = to_hex(block.prev);
+	if(block.proof) {
+		block.ksize = block.proof.ksize;
+		block.score = block.proof.score;
+		block.pool_key = to_hex(block.proof.pool_key);
+		block.farmer_key = to_hex(block.proof.farmer_key);
+	}
+	if(block.tx_base) {
+		block.reward = 0;
+		block.rewards = [];
+		for(const out of block.tx_base.outputs) {
+			block.reward += out.amount;
+			block.rewards.push({address: to_addr(out.address), amount: out.amount / 1e6});
+		}
+		block.reward /= 1e6;
+	}
+	for(const tx of block.tx_list) {
+		tx.id = to_hex(tx.id);
+		for(const input of tx.inputs) {
+			input.prev.txid = to_hex(input.prev.txid);
+		}
+		for(const out of tx.outputs) {
+			out.amount /= 1e6;
+			out.address = to_addr(out.address);
+			out.contract = to_addr(out.contract);
+		}
+	}
+	block.tx_count = block.tx_list.length;
+	return block
 }
 
 function on_block(res, ret)
@@ -32,19 +75,107 @@ function on_block(res, ret)
 		res.status(404);
 		return;
 	}
-	let block = ret.data;
-	block.hash = to_hex_str(block.hash);
-	block.prev = to_hex_str(block.prev);
-	if(block.proof) {
-		block.ksize = block.proof.ksize;
-		block.pool_key = to_hex_str(block.proof.pool_key);
-		block.farmer_key = to_hex_str(block.proof.farmer_key);
-	}
 	let args = {};
 	args.body = 'block';
-	args.block = block;
+	args.block = parse_block(ret.data);
 	res.render('index', args);
 }
+
+function gather_recent(blocks, height, res, ret)
+{
+	if(!ret.data) {
+		res.status(404);
+		return;
+	}
+	blocks.push(parse_block(ret.data));
+	
+	if(blocks.length < 30 && height > 0) {
+		height -= 1;
+		axios.get(host + '/api/node/get_block_at?height=' + height)
+			.then(gather_recent.bind(null, blocks, height, res))
+			.catch(on_error.bind(null, res));
+	} else {
+		let args = {};
+		args.body = 'recent';
+		args.blocks = blocks;
+		res.render('index', args);
+	}
+}
+
+function on_recent(res, ret)
+{
+	if(!ret.data) {
+		res.status(404);
+		return;
+	}
+	let height = ret.data;
+	var blocks = [];
+	axios.get(host + '/api/node/get_block_at?height=' + height)
+		.then(gather_recent.bind(null, blocks, height, res))
+		.catch(on_error.bind(null, res));
+}
+
+function on_address(res, address, balance, history)
+{
+	for(const entry of history) {
+		entry.amount = entry.amount / 1e6;
+		entry.address = to_addr(entry.address);
+		entry.contract = to_addr(entry.contract);
+	}
+	let args = {};
+	args.body = 'address';
+	args.address = address;
+	args.balance = balance / 1e6;
+	args.history = history.reverse();
+	res.render('index', args);
+}
+
+function on_transaction(res, tx, txi_info)
+{
+	tx.id = to_hex(tx.id);
+	tx.input_amount = 0;
+	tx.inputs.forEach((input, i) => {
+		if(i < txi_info.length) {
+			const info = txi_info[i];
+			if(info) {
+				input.amount = info.output.amount / 1e6;
+				input.address = to_addr(info.output.address);
+				input.contract = to_addr(info.output.contract);
+				if(input.contract == MMX_ADDR) {
+					tx.input_amount += info.output.amount;
+				}
+			}
+		}
+	});
+	tx.output_amount = 0;
+	for(const out of tx.outputs) {
+		out.address = to_addr(out.address);
+		out.contract = to_addr(out.contract);
+		if(out.contract == MMX_ADDR) {
+			tx.output_amount += out.amount;
+		}
+		out.amount = out.amount / 1e6;
+	}
+	tx.fee_amount = tx.input_amount - tx.output_amount;
+	tx.input_amount /= 1e6;
+	tx.output_amount /= 1e6;
+	tx.fee_amount /= 1e6;
+	
+	let args = {};
+	args.body = 'transaction';
+	args.tx = tx;
+	res.render('index', args);
+}
+
+app.get('/', (req, res) => {
+	res.redirect('/recent');
+});
+
+app.get('/recent', (req, res) => {
+	axios.get(host + '/api/node/get_height')
+		.then(on_recent.bind(null, res))
+		.catch(on_error.bind(null, res));
+});
 
 app.get('/block', (req, res) => {
 	if(req.query.hash) {
@@ -60,8 +191,55 @@ app.get('/block', (req, res) => {
 	}
 });
 
+app.get('/address', (req, res) => {
+	if(!req.query.addr) {
+		res.status(404).send();
+		return;
+	}
+	axios.get(host + '/api/node/get_balance?address=' + req.query.addr)
+		.then((ret) => {
+			const balance = ret.data;
+			axios.get(host + '/api/node/get_history_for?addresses=' + req.query.addr)
+			.then((ret) => {
+				on_address(res, req.query.addr, balance, ret.data);
+			})
+			.catch(on_error.bind(null, res));
+		})
+		.catch(on_error.bind(null, res));
+});
+
+app.get('/transaction', (req, res) => {
+	if(!req.query.id) {
+		res.status(404).send();
+		return;
+	}
+	axios.get(host + '/api/node/get_transaction?id=' + req.query.id)
+		.then((ret) => {
+			const tx = ret.data;
+			if(!tx) {
+				res.status(404).send();
+				return;
+			}
+			let keys = [];
+			for(const input of tx.inputs) {
+				keys.push(input.prev);
+			}
+			axios.post(host + '/api/node/get_txo_infos', {keys: keys})
+			.then((ret) => {
+				on_transaction(res, tx, ret.data);
+			})
+			.catch(on_error.bind(null, res));
+		})
+		.catch(on_error.bind(null, res));
+});
+
 app.use('/api', createProxyMiddleware({target: host, changeOrigin: true}));
 
 app.listen(port, '0.0.0.0', () => {
 	console.log(`Listening at http://0.0.0.0:${port}`);
 });
+
+
+
+
+
