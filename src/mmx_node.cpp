e35d41fbe631a536b641b05235474c0ae374579c
Max Wittal <max.wittal@mwittal.de>
2022-01-19 19:47:17 +0100

    dont run on testnet3

--- src/mmx_node.cpp
+++ src/mmx_node.cpp
@@ -33,8 +33,8 @@ int main(int argc, char** argv)
 
 	const auto params = mmx::get_params();
 
-	if(params->vdf_seed == "test1" || params->vdf_seed == "test2") {
-		vnx::log_error() << "This version is not compatible with testnet1/2, please remove NETWORK file and try again to switch to testnet3.";
+	if(params->vdf_seed == "test1" || params->vdf_seed == "test2" || params->vdf_seed == "test3") {
+		vnx::log_error() << "This version is not compatible with testnet 1/2/3, please remove NETWORK file and try again to switch to testnet4.";
 		vnx::close();
 		return -1;
 	}
