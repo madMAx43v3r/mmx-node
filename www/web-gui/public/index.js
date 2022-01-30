68fa70908d27a8d4222ff026e86d7923e3302784
Max Wittal <max.wittal@mwittal.de>
2022-01-30 02:36:21 +0100

    wallet summary

--- www/web-gui/public/index.js
+++ www/web-gui/public/index.js
@@ -2,41 +2,81 @@
 const app = Vue.createApp({
 })
 
+app.component('wallet-summary', {
+	data() {
+		return {
+			data: []
+		}
+	},
+	methods: {
+		update() {
+			fetch('/api/wallet/get_accounts')
+				.then(response => response.json())
+				.then(data => this.data = data);
+		}
+	},
+	created() {
+		this.update()
+	},
+	template: `<account-summary v-for="item in data" :index="item[0]" :account="item[1]"></account-summary>`
+})
+
+app.component('account-summary', {
+	props: {
+		index: Number,
+		account: Object
+	},
+	template: `
+		<div class="ui raised segment">
+			<div class="item">
+				<div class="ui horizontal label">#{{index}}</div>
+				{{account.name}}
+			</div>
+			<balance-table :index="index"></balance-table>
+		</div>
+		`
+})
+
 app.component('balance-table', {
-props: {
-	address: String
-},
-data() {
-	return {
-		balances: []
-	}
-},
-methods: {
-	update() {
-		fetch('/wapi/address?id=' + this.address)
-			.then(response => response.json())
-			.then(data => this.balances = data.balances);
-	}
-},
-created() {
-	this.update()
-},
-template: `
-	<table class="ui table">
-		<thead>
-		<tr>
-			<th>Balance</th>
-			<th>Token</th>
-		</tr>
-		</thead>
-		<tbody>
-		<tr v-for="(item, index) in balances">
-			<td>{{item.value}}</td>
-			<td>{{item.symbol}}</td>
-		</tr>
-		</tbody>
-	</table>
-	`
+	props: {
+		index: Number
+	},
+	data() {
+		return {
+			data: {
+				nfts: [],
+				balances: []
+			}
+		}
+	},
+	methods: {
+		update() {
+			fetch('/wapi/wallet/balance?index=' + this.index)
+				.then(response => response.json())
+				.then(data => this.data = data);
+		}
+	},
+	created() {
+		this.update()
+	},
+	template: `
+		<table class="ui table">
+			<thead>
+			<tr>
+				<th>Balance</th>
+				<th>Token</th>
+				<th>Contract</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr v-for="item in data.balances">
+				<td>{{item.value}}</td>
+				<td>{{item.symbol}}</td>
+				<td>{{item.contract ? item.contract : ""}}</td>
+			</tr>
+			</tbody>
+		</table>
+		`
 })
 
 app.mount('#content');
