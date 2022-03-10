cf8c1459a750fd426607cad82102a79d0255c7b4
Max Wittal <max.wittal@mwittal.de>
2022-03-10 22:31:20 +0100

    Token transfer()

--- src/contract/Token.cpp
+++ src/contract/Token.cpp
@@ -104,6 +104,11 @@ std::vector<tx_out_t> Token::validate(std::shared_ptr<const Operation> operation
 	return {};
 }
 
+void Token::transfer(const vnx::optional<addr_t>& new_owner)
+{
+	owner = new_owner;
+}
+
 
 } // contract
 } // mmx
