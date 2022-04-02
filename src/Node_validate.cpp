b261e16fdc01e2d2701eeed8af4691f54b7e1abe
Max Wittal <max.wittal@mwittal.de>
2022-04-02 13:55:16 +0200

    refactor

--- src/Node_validate.cpp
+++ src/Node_validate.cpp
@@ -211,7 +211,6 @@ std::shared_ptr<const Transaction> Node::validate(	std::shared_ptr<const Transac
 			}
 		}
 
-		// TODO: parallel for if many inputs
 		for(const auto& in : tx->inputs)
 		{
 			auto iter = utxo_map.find(in.prev);
@@ -225,16 +224,18 @@ std::shared_ptr<const Transaction> Node::validate(	std::shared_ptr<const Transac
 				throw std::logic_error("missing solution");
 			}
 			std::shared_ptr<const Contract> contract;
+			std::shared_ptr<const Context> tx_context = context;
 
 			if(in.flags & tx_in_t::IS_EXEC) {
 				contract = get_contract(utxo.address);
+				if(!contract) {
+					throw std::logic_error("no such contract");
+				}
+				tx_context = create_context_for_tx(context, contract, tx);
 			} else {
 				auto pubkey = contract::PubKey::create();
 				pubkey->address = utxo.address;
 				contract = pubkey;
-			}
-			if(!contract) {
-				throw std::logic_error("no such contract");
 			}
 			auto spend = operation::Spend::create();
 			spend->address = utxo.address;
@@ -242,7 +243,7 @@ std::shared_ptr<const Transaction> Node::validate(	std::shared_ptr<const Transac
 			spend->key = in.prev;
 			spend->utxo = utxo;
 
-			const auto outputs = contract->validate(spend, create_context_for_tx(context, contract, tx));
+			const auto outputs = contract->validate(spend, tx_context);
 			exec_outputs.insert(exec_outputs.end(), outputs.begin(), outputs.end());
 
 			amounts[utxo.contract] += utxo.amount;
