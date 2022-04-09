31587de7a287d7bc5e853b2579c0dd2965e0cbe3
Max Wittal <max.wittal@mwittal.de>
2022-04-09 12:56:24 +0200

    generated

--- generated/include/mmx/WebAPIBase.hxx
+++ generated/include/mmx/WebAPIBase.hxx
@@ -23,6 +23,7 @@ public:
 	std::string node_server = "Node";
 	std::string wallet_server = "Wallet";
 	std::string exchange_server = "ExchClient";
+	std::string config_path;
 	uint32_t max_log_history = 10000;
 	
 	typedef ::vnx::Module Super;
@@ -74,13 +75,14 @@ protected:
 
 template<typename T>
 void WebAPIBase::accept_generic(T& _visitor) const {
-	_visitor.template type_begin<WebAPIBase>(5);
+	_visitor.template type_begin<WebAPIBase>(6);
 	_visitor.type_field("input_blocks", 0); _visitor.accept(input_blocks);
 	_visitor.type_field("node_server", 1); _visitor.accept(node_server);
 	_visitor.type_field("wallet_server", 2); _visitor.accept(wallet_server);
 	_visitor.type_field("exchange_server", 3); _visitor.accept(exchange_server);
-	_visitor.type_field("max_log_history", 4); _visitor.accept(max_log_history);
-	_visitor.template type_end<WebAPIBase>(5);
+	_visitor.type_field("config_path", 4); _visitor.accept(config_path);
+	_visitor.type_field("max_log_history", 5); _visitor.accept(max_log_history);
+	_visitor.template type_end<WebAPIBase>(6);
 }
 
 
