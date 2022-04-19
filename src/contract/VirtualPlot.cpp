96938eaf48fcb1affc10faa385f00d913b18ff53
Max Wittal <max.wittal@mwittal.de>
2022-04-20 00:44:36 +0200

    VirtualPlot update

--- src/contract/VirtualPlot.cpp
+++ src/contract/VirtualPlot.cpp
@@ -14,7 +14,7 @@ namespace contract {
 
 vnx::bool_t VirtualPlot::is_valid() const
 {
-	return Super::is_valid() && farmer_key != bls_pubkey_t() && (!pool_address || *pool_address != addr_t());
+	return Super::is_valid() && farmer_key != bls_pubkey_t() && (!reward_address || *reward_address != addr_t());
 }
 
 hash_t VirtualPlot::calc_hash() const
@@ -26,7 +26,7 @@ hash_t VirtualPlot::calc_hash() const
 	write_bytes(out, get_type_hash());
 	write_field(out, "version", 		version);
 	write_field(out, "farmer_key", 		farmer_key);
-	write_field(out, "pool_address", 	pool_address);
+	write_field(out, "reward_address", 	reward_address);
 	out.flush();
 
 	return hash_t(buffer);
@@ -34,6 +34,10 @@ hash_t VirtualPlot::calc_hash() const
 
 uint64_t VirtualPlot::calc_cost(std::shared_ptr<const ChainParams> params) const {
 	return 0;
+}
+
+vnx::bool_t VirtualPlot::is_spendable(const utxo_t& utxo, std::shared_ptr<const Context> context) const {
+	return false;
 }
 
 void VirtualPlot::bls_transfer(const bls_pubkey_t& new_farmer_key)
