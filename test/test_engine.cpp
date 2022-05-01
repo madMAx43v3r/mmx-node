d256000a467ed04dd595c49a371e76b40b1009b4
Max Wittal <max.wittal@mwittal.de>
2022-05-01 23:50:22 +0200

    test_engine

---
+++ test/test_engine.cpp
@@ -0,0 +1,38 @@
+/*
+ * test_engine.cpp
+ *
+ *  Created on: May 1, 2022
+ *      Author: mad
+ */
+
+#include <mmx/vm/Engine.h>
+#include <mmx/vm/StorageRAM.h>
+#include <mmx/vm/StorageProxy.h>
+
+#include <iostream>
+
+using namespace mmx;
+
+
+int main(int arcv, char** argv)
+{
+	auto backend = std::make_shared<vm::StorageRAM>();
+	{
+		vm::Engine engine(addr_t(), backend, false);
+		engine.assign(vm::MEM_CONST + 0, new vm::var_t());
+		engine.assign(vm::MEM_CONST + 1, new vm::uint_t(1337));
+
+		auto& code = engine.code;
+		code.emplace_back(vm::OP_NOP);
+		code.emplace_back(vm::OP_COPY, 0, vm::MEM_STACK + 0, vm::MEM_CONST + 1);
+		code.emplace_back(vm::OP_RET);
+
+		engine.credits = 10000;
+		engine.begin(0);
+		engine.run();
+		std::cout << vm::to_string(engine.read(vm::MEM_STACK + 0)) << std::endl;
+	}
+	return 0;
+}
+
+
