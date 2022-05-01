b1cadff3c1722c53506e445abaa84bc18c6b76db
Max Wittal <max.wittal@mwittal.de>
2022-05-01 22:51:59 +0200

    uint128 refactor

--- include/mmx/uint128.hpp
+++ include/mmx/uint128.hpp
@@ -14,6 +14,8 @@
 #include <vnx/Output.hpp>
 #include <vnx/Variant.hpp>
 
+#include <algorithm>
+
 
 namespace mmx {
 
@@ -35,54 +37,7 @@ struct uint128 : uint128_t {
 
 namespace vnx {
 
-inline
-void read(vnx::TypeInput& in, mmx::uint128& value, const vnx::TypeCode* type_code, const uint16_t* code)
-{
-	if(sizeof(value) != 16) {
-		throw std::logic_error("sizeof(uint128) != 16");
-	}
-	switch(code[0]) {
-		case CODE_STRING:
-		case CODE_ALT_STRING: {
-			std::string str;
-			vnx::read(in, str, type_code, code);
-			try {
-				value = uint128_t(str, 10);
-			} catch(...) {
-				value = uint128_0;
-			}
-			break;
-		}
-		case CODE_UINT64:
-		case CODE_ALT_UINT64: {
-			uint64_t tmp = 0;
-			vnx::read(in, tmp, type_code, code);
-			value = uint128_t(tmp);
-			break;
-		}
-		case CODE_DYNAMIC:
-		case CODE_ALT_DYNAMIC: {
-			vnx::Variant tmp;
-			vnx::read(in, tmp, type_code, code);
-			if(tmp.is_string()) {
-				std::string str;
-				tmp.to(str);
-				try {
-					value = uint128_t(str, 10);
-				} catch(...) {
-					value = uint128_0;
-				}
-			} else {
-				std::array<uint8_t, 16> array = {};
-				tmp.to(array);
-				::memcpy(&value, array.data(), array.size());
-			}
-			break;
-		}
-		default:
-			vnx::read(in, reinterpret_cast<std::array<uint8_t, 16>&>(value), type_code, code);
-	}
-}
+void read(vnx::TypeInput& in, mmx::uint128& value, const vnx::TypeCode* type_code, const uint16_t* code);
 
 inline
 void write(vnx::TypeOutput& out, const mmx::uint128& value, const vnx::TypeCode* type_code = nullptr, const uint16_t* code = nullptr)
