bbcfd9861fa2f6f361564c2bd7b10dc314712508
Max Wittal <max.wittal@mwittal.de>
2022-05-06 00:47:15 +0200

    StorageCache

---
+++ include/mmx/vm/StorageCache.h
@@ -0,0 +1,42 @@
+/*
+ * StorageCache.h
+ *
+ *  Created on: May 5, 2022
+ *      Author: mad
+ */
+
+#ifndef INCLUDE_MMX_VM_STORAGECACHE_H_
+#define INCLUDE_MMX_VM_STORAGECACHE_H_
+
+#include <mmx/vm/StorageRAM.h>
+
+
+namespace mmx {
+namespace vm {
+
+class StorageCache : public StorageRAM {
+public:
+	typedef StorageRAM Super;
+
+	StorageCache(std::shared_ptr<Storage> backend);
+
+	~StorageCache();
+
+	var_t* read(const addr_t& contract, const uint64_t src) const override;
+
+	var_t* read(const addr_t& contract, const uint64_t src, const uint64_t key) const override;
+
+	uint64_t lookup(const addr_t& contract, const var_t& value) const override;
+
+	void commit();
+
+private:
+	std::shared_ptr<Storage> backend;
+
+};
+
+
+} // vm
+} // mmx
+
+#endif /* INCLUDE_MMX_VM_STORAGECACHE_H_ */
