9cb010ddd741939bb17347521e313447607f96a0
Max Wittal <max.wittal@mwittal.de>
2022-05-17 14:42:58 +0200

    clear stack above args

--- src/vm_interface.cpp
+++ src/vm_interface.cpp
@@ -351,6 +351,7 @@ void set_args(std::shared_ptr<vm::Engine> engine, const std::vector<vnx::Variant
 
 void execute(std::shared_ptr<vm::Engine> engine, const contract::method_t& method)
 {
+	engine->clear_stack(1 + method.args.size());
 	engine->begin(method.entry_point);
 	engine->run();
 
