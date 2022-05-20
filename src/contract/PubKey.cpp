ae5bb654404d3ec576dcccbdcadbbd1f2ff0f6e1
Max Wittal <max.wittal@mwittal.de>
2022-05-20 22:36:36 +0200

    forgotten functions

--- src/contract/PubKey.cpp
+++ src/contract/PubKey.cpp
@@ -32,6 +32,10 @@ hash_t PubKey::calc_hash() const
 	return hash_t(buffer);
 }
 
+uint64_t PubKey::calc_cost(std::shared_ptr<const ChainParams> params) const {
+	return 0;
+}
+
 std::vector<addr_t> PubKey::get_dependency() const {
 	return {};
 }
