f5448354506ff382053d57b054d5ea4e90e40eff
stotiks <47013208+stotiks@users.noreply.github.com>
2022-05-31 05:59:16 +0300

    github action for win

---
+++ cmake/custom-triplets/x64-windows-mmx.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+
+if(PORT MATCHES "gmp" OR PORT MATCHES "secp256k1")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
---
+++ make_release.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+IF "%VCPKG_ROOT%"=="" (
+	SET VCPKG_ROOT=C:/dev/vcpkg
+)
+
+IF "%CMAKE_BUILD_PRESET%"=="" (
+	SET CMAKE_BUILD_PRESET=windows-release
+)
+
+cmake --preset %CMAKE_BUILD_PRESET% && ^
+cmake --build --preset %CMAKE_BUILD_PRESET%
