a29ceddeda37657e10730ce9051e2d2822d75ffb
Max Wittal <max.wittal@mwittal.de>
2022-06-09 00:06:12 +0200

    working on db

--- include/mmx/DataBase.h
+++ include/mmx/DataBase.h
@@ -8,7 +8,10 @@
 #ifndef INCLUDE_MMX_DB_DATABASE_H_
 #define INCLUDE_MMX_DB_DATABASE_H_
 
+#include <mmx/TableIndex.hxx>
+
 #include <vnx/File.h>
+#include <fstream>
 
 
 namespace mmx {
@@ -46,6 +49,31 @@ struct db_val_t {
 };
 
 class Table {
+protected:
+	struct block_t {
+		uint32_t level = 0;
+		uint32_t min_version = 0;
+		uint32_t max_version = 0;
+		std::string name;
+		std::vector<int64_t> index;
+	};
+
+	struct key_t {
+		uint32_t version = -1;
+		std::shared_ptr<db_val_t> data;
+	};
+
+	struct mem_compare_t {
+		Table* table = nullptr;
+		mem_compare_t(Table* table) : table(table) {}
+		bool operator()(const std::pair<std::shared_ptr<db_val_t>, uint32_t>& lhs, const std::pair<std::shared_ptr<db_val_t>, uint32_t>& rhs) const {
+			if(*lhs.first == *rhs.first) {
+				return lhs.second < rhs.second;
+			}
+			return table->comparator(*lhs.first, *rhs.first);
+		}
+	};
+
 public:
 	uint32_t level_factor = 4;
 	uint64_t max_block_size = 16 * 1024 * 1024;
@@ -65,35 +93,37 @@ public:
 
 	void revert(const uint32_t new_version);
 
+	uint32_t current_version() const {
+		return index->version;
+	}
+
+	class Iterator {
+	public:
+		Iterator(std::shared_ptr<const Table> table);
+	private:
+		std::shared_ptr<const Table> table;
+		std::map<std::pair<std::shared_ptr<db_val_t>, uint32_t>, std::shared_ptr<vnx::File>, mem_compare_t> block_map;
+	};
+
 private:
-	struct block_t {
-		uint32_t level = 0;
-		std::shared_ptr<vnx::File> file;
-	};
+	void read_entry(vnx::TypeInput& in, uint32_t& version, std::shared_ptr<db_val_t>& key, std::shared_ptr<db_val_t>& value);
 
-	struct key_t {
-		uint32_t version = -1;
-		std::shared_ptr<db_val_t> data;
-	};
+	void write_entry(vnx::TypeOutput& out, uint32_t version, std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> value);
 
-	struct mem_compare_t {
-		Table* table = nullptr;
-		mem_compare_t(Table* table) : table(table) {}
-		bool operator()(const std::pair<std::shared_ptr<db_val_t>, uint32_t>& lhs, const std::pair<std::shared_ptr<db_val_t>, uint32_t>& rhs) const {
-			if(*lhs.first == *rhs.first) {
-				return lhs.second < rhs.second;
-			}
-			return table->comparator(*lhs.first, *rhs.first);
-		}
-	};
+	void insert_entry(uint32_t version, std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> value);
+
+	std::shared_ptr<block_t> read_block(const std::string& name) const;
 
 	void flush();
 
-	uint32_t version = 0;
-	uint64_t mem_block_size = 0;
+	void write_index();
 
+	std::ofstream debug_log;
+	std::shared_ptr<TableIndex> index;
 	std::shared_ptr<vnx::File> write_log;
-	std::vector<std::shared_ptr<block_t>> block_list;
+	std::list<std::shared_ptr<block_t>> blocks;
+
+	uint64_t mem_block_size = 0;
 	std::map<std::pair<std::shared_ptr<db_val_t>, uint32_t>, std::shared_ptr<db_val_t>, mem_compare_t> mem_block;
 
 };
--- src/DataBase.cpp
+++ src/DataBase.cpp
@@ -12,6 +12,13 @@
 
 namespace mmx {
 
+template<typename T>
+std::string to_number(const T& value, const size_t n_zero)
+{
+	const auto tmp = std::to_string(value);
+	return std::string(n_zero - std::min(n_zero, tmp.length()), '0') + tmp;
+}
+
 const std::function<bool(const db_val_t&, const db_val_t&)> Table::default_comparator =
 	[](const db_val_t& lhs, const db_val_t& rhs) -> bool {
 		if(lhs.size == rhs.size) {
@@ -23,6 +30,103 @@ const std::function<bool(const db_val_t&, const db_val_t&)> Table::default_compa
 Table::Table(const std::string& file_path, const std::function<bool(const db_val_t&, const db_val_t&)>& comparator)
 	:	file_path(file_path), comparator(comparator), mem_block(mem_compare_t(this))
 {
+	debug_log.open(file_path + "debug.log", std::ios_base::app);
+
+	index = vnx::read_from_file<TableIndex>(file_path + "index.dat");
+	if(!index) {
+		index = TableIndex::create();
+		debug_log << "Initialized table" << std::endl;
+	} else {
+		debug_log << "Loaded table at version " << index->version << std::endl;
+	}
+	for(const auto& name : index->blocks) {
+		blocks.push_back(read_block(name));
+	}
+//	for(const auto& name : index->deleted) {
+//		vnx::File(file_path + name).remove();
+//	}
+//	index->deleted.clear();
+
+	// purge left-over data
+	revert(index->version);
+
+	write_log->open(file_path + "write_log.dat", "ab");
+	write_log->open("rb+");
+	{
+		auto& in = write_log->in;
+		auto offset = in.get_input_pos();
+		const auto min_version = blocks.empty() ? 0 : blocks.back()->max_version + 1;
+		while(true) {
+			try {
+				uint32_t version;
+				std::shared_ptr<db_val_t> key;
+				std::shared_ptr<db_val_t> value;
+				read_entry(write_log->in, version, key, value);
+				if(version >= min_version && version <= index->version) {
+					insert_entry(version, key, value);
+				}
+				offset = in.get_input_pos();
+			}
+			catch(const std::underflow_error& ex) {
+				break;
+			}
+			catch(const std::exception& ex) {
+				debug_log << "Reading from write_log.dat failed with: " << ex.what() << std::endl;
+			}
+		}
+		write_log->seek_to(offset);
+		debug_log << "Loaded " << mem_block.size() << " entries from write_log.dat" << std::endl;
+	}
+}
+
+std::shared_ptr<Table::block_t> Table::read_block(const std::string& name) const
+{
+	auto block = std::make_shared<block_t>();
+	block->name = name;
+
+	vnx::File file(file_path + name);
+	auto& in = file.in;
+
+	uint16_t format = 0;
+	vnx::read(in, format);
+	if(format != 0) {
+		throw std::runtime_error("invalid block format: " + std::to_string(format));
+	}
+	vnx::read(in, block->level);
+	vnx::read(in, block->min_version);
+	vnx::read(in, block->max_version);
+
+	uint64_t index_size = 0;
+	vnx::read(in, index_size);
+	block->index.resize(index_size);
+	in.read(block->index.data(), block->index.size() * 8);
+	return block;
+}
+
+void Table::read_entry(vnx::TypeInput& in, uint32_t& version, std::shared_ptr<db_val_t>& key, std::shared_ptr<db_val_t>& value)
+{
+	vnx::read(in, version);
+	{
+		uint32_t size = 0;
+		vnx::read(in, size);
+		key = std::make_shared<db_val_t>(size);
+		in.read(key->data, key->size);
+	}
+	{
+		uint32_t size = 0;
+		vnx::read(in, size);
+		value = std::make_shared<db_val_t>(size);
+		in.read(value->data, value->size);
+	}
+}
+
+void Table::write_entry(vnx::TypeOutput& out, uint32_t version, std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> value)
+{
+	vnx::write(out, version);
+	vnx::write(out, key->size);
+	out.write(key->data, key->size);
+	vnx::write(out, value->size);
+	out.write(value->data, value->size);
 }
 
 void Table::insert(std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> value)
@@ -30,13 +134,12 @@ void Table::insert(std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> valu
 	if(!key || !value) {
 		throw std::logic_error("!key || !value");
 	}
-	auto& out = write_log->out;
-	vnx::write(out, version);
-	vnx::write(out, key->size);
-	out.write(key->data, key->size);
-	vnx::write(out, value->size);
-	out.write(value->data, value->size);
+	write_entry(write_log->out, index->version, key, value);
+	insert_entry(index->version, key, value);
+}
 
+void Table::insert_entry(uint32_t version, std::shared_ptr<db_val_t> key, std::shared_ptr<db_val_t> value)
+{
 	auto& entry = mem_block[std::make_pair(key, version)];
 	if(entry) {
 		mem_block_size -= key->size + entry->size;
@@ -59,7 +162,7 @@ std::shared_ptr<db_val_t> Table::find(std::shared_ptr<db_val_t> key) const
 
 void Table::commit(const uint32_t new_version)
 {
-	if(new_version <= version) {
+	if(new_version <= index->version) {
 		throw std::logic_error("commit(): new version <= current version");
 	}
 	write_log->flush();
@@ -67,27 +170,100 @@ void Table::commit(const uint32_t new_version)
 	if(mem_block_size >= max_block_size) {
 		flush();
 	}
-	version = new_version;
+	index->version = new_version;
+	write_index();
 }
 
 void Table::revert(const uint32_t new_version)
 {
-	if(new_version > version) {
+	if(new_version > index->version) {
 		throw std::logic_error("revert(): new version > current version");
 	}
+	index->version = new_version;
+	write_index();
+
+	for(auto iter = blocks.begin(); iter != blocks.end();) {
+		const auto& block = *iter;
+		if(block->min_version >= new_version) {
+			// delete block
+			vnx::File(file_path + block->name).remove();
+			iter = blocks.erase(iter);
+		} else if(block->max_version >= new_version) {
+			// update max version
+			vnx::File file(file_path + block->name);
+			file.seek_to(2 + 4 + 8);
+			block->max_version = new_version - 1;
+			vnx::write(file.out, block->max_version);
+			iter++;
+		} else {
+			iter++;
+		}
+	}
+	index->blocks.clear();
+	for(const auto& block : blocks) {
+		index->blocks.push_back(block->name);
+	}
 	for(auto iter = mem_block.begin(); iter != mem_block.end();) {
-		if(iter->first.second > new_version) {
+		const auto& key = iter->first;
+		if(key.second >= new_version) {
+			mem_block_size -= key.first->size + iter->second->size;
 			iter = mem_block.erase(iter);
 		} else {
 			iter++;
 		}
 	}
+	write_index();
 }
 
 void Table::flush()
 {
-	// TODO
+	if(mem_block.empty()) {
+		return;
+	}
+	auto block = std::make_shared<block_t>();
+	block->min_version = -1;
+	block->name = to_number(index->next_block_id++, 6) + ".dat";
+	block->index.reserve(mem_block.size());
+
+	vnx::File file(file_path + block->name);
+	file.open("wb+");
+
+	auto& out = file.out;
+	file.seek_to(2 + 20 + mem_block.size() * 8);
+	for(const auto& entry : mem_block) {
+		const auto& version = entry.first.second;
+		block->min_version = std::min(version, block->min_version);
+		block->max_version = std::max(version, block->max_version);
+		block->index.push_back(out.get_output_pos());
+		write_entry(out, version, entry.first.first, entry.second);
+	}
+	file.seek_begin();
+	vnx::write(out, uint16_t(0));
+	vnx::write(out, block->level);
+	vnx::write(out, block->min_version);
+	vnx::write(out, block->max_version);
+	vnx::write(out, uint64_t(block->index.size()));
+	out.write(block->index.data(), block->index.size() * 8);
+	file.close();
+
+	mem_block.clear();
+	mem_block_size = 0;
+	blocks.push_back(block);
+
+	index->blocks.push_back(block->name);
+	write_index();
+
+	// clear log after writing index
+	write_log->open("wb");
 }
+
+void Table::write_index()
+{
+	vnx::write_to_file(file_path + "index.dat", index);
+}
+
+
+
 
 
 } // mmx
