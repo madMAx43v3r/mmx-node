3a645c94ec7b70338d3d9489b711ae3c9fa1638a
Max Wittal <max.wittal@mwittal.de>
2022-07-20 00:08:50 +0200

    using sha256_ni if available

--- src/hash_t.cpp
+++ src/hash_t.cpp
@@ -9,13 +9,43 @@
 
 #include <bls.hpp>
 #include <sodium.h>
+#include <sha256_ni.h>
+
+
+static bool check_cpuid_sha_ni()
+{
+	int a, b, c, d;
+
+	// Look for CPUID.7.0.EBX[29]
+	// EAX = 7, ECX = 0
+	a = 7;
+	c = 0;
+
+	asm volatile ("cpuid"
+		:"=a"(a), "=b"(b), "=c"(c), "=d"(d)
+		:"a"(a), "c"(c)
+	);
+
+	// Intel® SHA Extensions feature bit is EBX[29]
+	return ((b >> 29) & 1);
+}
 
 
 namespace mmx {
 
 hash_t::hash_t(const void* data, const size_t num_bytes)
 {
-	bls::Util::Hash256(bytes.data(), (const uint8_t*)data, num_bytes);
+	static bool have_init = false;
+	static bool have_sha_ni = false;
+	if(!have_init) {
+		have_init = true;
+		have_sha_ni = check_cpuid_sha_ni();
+	}
+	if(have_sha_ni) {
+		sha256_ni(bytes.data(), (const uint8_t*)data, num_bytes);
+	} else {
+		bls::Util::Hash256(bytes.data(), (const uint8_t*)data, num_bytes);
+	}
 }
 
 hash_t hash_t::random()
