cf5853dfe340a83d60201aade435dd260ac37a0b
Max Wittal <max.wittal@mwittal.de>
2022-07-20 00:33:52 +0200

    fallback for AVX2

--- include/sha256_avx2.h
+++ include/sha256_avx2.h
@@ -10,7 +10,9 @@
 
 #include <cstdint>
 
-void sha256_avx2_64_x8(uint8_t* out, uint8_t* in, const uint32_t length);
+void sha256_avx2_64_x8(uint8_t* out, uint8_t* in, const uint64_t length);
+
+void sha256_64_x8(uint8_t* out, uint8_t* in, const uint64_t length);
 
 
 #endif /* INCLUDE_SHA256_AVX2_H_ */
