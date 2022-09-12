7e4a63f09e30021a66ea6ac460d8e7ac9e6884f2
Max Wittal <max.wittal@mwittal.de>
2022-09-12 19:28:41 +0200

    added mnemonic wordlist

---
+++ include/mmx/mnemonic.h
@@ -0,0 +1,30 @@
+/*
+ * mnemonic.h
+ *
+ *  Created on: Sep 12, 2022
+ *      Author: mad
+ */
+
+#ifndef INCLUDE_MMX_MNEMONIC_H_
+#define INCLUDE_MMX_MNEMONIC_H_
+
+#include <mmx/hash_t.hpp>
+
+#include <string>
+#include <vector>
+
+
+namespace mmx {
+namespace mnemonic {
+
+extern const std::vector<std::string> wordlist_en;
+
+std::vector<std::string> seed_to_words_en(const hash_t& seed, const std::vector<std::string>& wordlist = wordlist_en);
+
+hash_t words_to_seed(const std::vector<std::string>& words, const std::vector<std::string>& wordlist);
+
+
+} // mnemonic
+} // mmx
+
+#endif /* INCLUDE_MMX_MNEMONIC_H_ */
