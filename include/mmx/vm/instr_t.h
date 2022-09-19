b058f25165ff1a9834db7b26dfc146419201a5d7
Max Wittal <max.wittal@mwittal.de>
2022-09-19 16:25:15 +0200

    ops todo

--- include/mmx/vm/instr_t.h
+++ include/mmx/vm/instr_t.h
@@ -78,10 +78,11 @@ enum opcode_e : uint8_t {
 	OP_LOG = 0xC0,	// level, message
 	OP_SEND,		// address, amount, currency
 	OP_MINT,		// address, amount
+	OP_MINT_NFT,	// TODO: address, data
 	OP_EVENT,		// name, data
 	OP_FAIL,		// message
 	OP_RCALL,		// name, method, stack_ptr, nargs
-	OP_DEPOSIT,		// TODO
+	OP_DEPOSIT,		// TODO: name, method, stack_ptr, nargs
 
 };
 
