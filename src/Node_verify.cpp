15a139204eab0e8584bb65d86a08c7a4fedad2b4
Max Wittal <max.wittal@mwittal.de>
2022-09-28 19:26:31 +0200

    Revert "Node::verify() fix"
    This reverts commit c276a025b5a20ce9a05710a8f918edb5bad69de0.

--- src/Node_verify.cpp
+++ src/Node_verify.cpp
@@ -73,7 +73,6 @@ bool Node::verify(std::shared_ptr<const ProofResponse> value)
 	}
 	catch(const std::exception& ex) {
 		log(WARN) << "Got invalid proof: " << ex.what();
-		return false;
 	}
 	return true;
 }
