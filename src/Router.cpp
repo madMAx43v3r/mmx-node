7c464480a09d86b1e72568960335be57c4119c67
Max Wittal <max.wittal@mwittal.de>
2022-10-01 21:35:19 +0200

    Router crash fix

--- src/Router.cpp
+++ src/Router.cpp
@@ -387,7 +387,7 @@ void Router::update()
 		peer->pending_cost = std::max<double>(peer->pending_cost, 0) * 0.99;
 
 		// clear old hashes
-		while(peer->sent_hashes.size() > max_sent_cache) {
+		while(peer->hash_queue.size() > max_sent_cache) {
 			peer->sent_hashes.erase(peer->hash_queue.front());
 			peer->hash_queue.pop();
 		}
@@ -410,8 +410,7 @@ void Router::update()
 
 	// clear seen hashes
 	while(hash_queue.size() > max_hash_cache) {
-		const auto hash = hash_queue.front();
-		hash_info.erase(hash);
+		hash_info.erase(hash_queue.front());
 		hash_queue.pop();
 	}
 
@@ -1079,8 +1078,9 @@ void Router::send()
 			const auto& item = iter->second;
 			if(!peer->sent_hashes.count(item.hash)) {
 				if(send_to(peer, item.value, item.reliable)) {
-					peer->sent_hashes.insert(item.hash);
-					peer->hash_queue.push(item.hash);
+					if(peer->sent_hashes.insert(item.hash).second) {
+						peer->hash_queue.push(item.hash);
+					}
 				}
 			}
 			iter = peer->send_queue.erase(iter);
