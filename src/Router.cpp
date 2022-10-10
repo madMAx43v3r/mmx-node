b8bcc1cddfbea0096cf8a64367ac74221486150c
Max Wittal <max.wittal@mwittal.de>
2022-10-10 22:25:02 +0200

    Router stop connecting once max_peer_set reached

--- src/Router.cpp
+++ src/Router.cpp
@@ -705,6 +705,7 @@ void Router::connect()
 	}
 
 	// connect to new peers
+	if(synced_peers.size() < num_peers_out || peer_set.size() < max_peer_set)
 	{
 		std::set<std::string> try_peers;
 		std::set<std::string> all_peers = peer_set;
@@ -855,7 +856,7 @@ void Router::discover()
 		return;
 	}
 	auto method = Router_get_peers::create();
-	method->max_count = max_peer_set / num_peers_out;
+	method->max_count = num_peers_out;
 	auto req = Request::create();
 	req->id = next_request_id++;
 	req->method = method;
