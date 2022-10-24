46a9a734abb72d308d2b271d328713972c3f53ca
Max Wittal <max.wittal@mwittal.de>
2022-10-24 11:06:24 +0200

    Harvester avoid infinite recursion

--- src/Harvester.cpp
+++ src/Harvester.cpp
@@ -261,9 +261,12 @@ void Harvester::find_plot_dirs(const std::set<std::string>& dirs, std::set<std::
 				}
 			}
 			for(const auto& sub_dir : dir.directories()) {
-				const auto name = sub_dir->get_name();
-				if(name != "System Volume Information" && name != "$RECYCLE.BIN") {
-					sub_dirs.insert(sub_dir->get_path());
+				const auto path = sub_dir->get_path();
+				if(!all_dirs.count(path)) {
+					const auto name = sub_dir->get_name();
+					if(name != "System Volume Information" && name != "$RECYCLE.BIN" && name != "lost+found") {
+						sub_dirs.insert(sub_dir->get_path());
+					}
 				}
 			}
 		} catch(const std::exception& ex) {
