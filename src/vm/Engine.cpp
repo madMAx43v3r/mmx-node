543ef740de030a311d630df21e9005ab78d0ae24
Max Wittal <max.wittal@mwittal.de>
2023-01-19 15:30:15 +0100

    error code msg fix

--- src/vm/Engine.cpp
+++ src/vm/Engine.cpp
@@ -1516,7 +1516,7 @@ void Engine::exec(const instr_t& instr)
 		error_code = instr.b;
 		throw std::runtime_error("failed with: "
 				+ to_string_value(read(deref_addr(instr.a, instr.flags & OPFLAG_REF_A)))
-				+ (instr.b ? " (code " + std::to_string(instr.b) : ""));
+				+ (instr.b ? " (code " + std::to_string(instr.b) + ")" : ""));
 	case OP_RCALL:
 		rcall(	deref_addr(instr.a, instr.flags & OPFLAG_REF_A),
 				deref_addr(instr.b, instr.flags & OPFLAG_REF_B),
