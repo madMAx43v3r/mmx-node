e3b646290e83c5f4a173febe592616564eabf69f
stotiks <47013208+stotiks@users.noreply.github.com>
2023-02-09 04:01:39 +0200

    MMX_GIGAHORSE

--- cmake/win_pack.cmake
+++ cmake/win_pack.cmake
@@ -22,6 +22,7 @@ if(NOT MMX_VERSION MATCHES "^v([0-9]+)\\.([0-9]+)\\.([0-9]+)$")
 endif()
 
 message(STATUS "MMX_VERSION=${MMX_VERSION}")
+message(STATUS "MMX_GIGAHORSE=${MMX_GIGAHORSE}")
 
 #file(STRINGS "include/mmx/version.h" MMX_VERSION_H REGEX "^#define MMX_VERSION \"[^\"]*\"$")
 string(REGEX REPLACE "^v([0-9]+).*$" "\\1" MMX_VERSION_MAJOR "${MMX_VERSION}")
@@ -38,7 +39,12 @@ set(CPACK_PACKAGE_VERSION_PATCH ${MMX_VERSION_PATCH})
 
 include(cmake/product_version/generate_product_version.cmake)
 set(MMX_ICON "${CMAKE_CURRENT_SOURCE_DIR}/cmake/mmx.ico")
-set(MMX_FRIENDLY_STRING "MMX Node ${MMX_VERSION}")
+
+if (${MMX_GIGAHORSE} STREQUAL "TRUE")
+	set(MMX_BUNDLE "MMX Node (Gigahorse)")
+else()
+	set(MMX_BUNDLE "MMX Node (Classic)")
+endif()
 
 list(APPEND APP_FILES
 	mmx mmx_node mmx_farmer mmx_wallet mmx_timelord mmx_harvester
@@ -57,7 +63,7 @@ foreach(APPFILE IN LISTS APP_FILES TOOL_FILES)
 	generate_product_version(
 		${ProductVersionFiles}
 		NAME ${APPFILE}
-		BUNDLE "MMX Node"
+		BUNDLE ${MMX_BUNDLE}
 		COMPANY_NAME "madMAx43v3r"
 		FILE_DESCRIPTION ${APPFILE}
 		ICON ${MMX_ICON}
@@ -68,7 +74,7 @@ foreach(APPFILE IN LISTS APP_FILES TOOL_FILES)
 	target_sources(${APPFILE} PRIVATE ${${ProductVersionFiles}})
 endforeach()
 
-if ("${MMX_WIN_PACK}" STREQUAL "TRUE")
+if (${MMX_WIN_PACK} STREQUAL "TRUE")
 
 set(CMAKE_INSTALL_SYSTEM_RUNTIME_COMPONENT libraries)
 set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION ./)
@@ -144,28 +150,30 @@ install(FILES ${MMX_PLOTTER_K32_PATH} DESTINATION ./ RENAME mmx_plot.exe COMPONE
 install(FILES ${MMX_PLOTTER_K34_PATH} DESTINATION ./ RENAME mmx_plot_k34.exe COMPONENT plotters)
 install(FILES ${PLOTTER_BLADEBIT_PATH} DESTINATION ./ RENAME mmx_bladebit.exe COMPONENT plotters)
 
+if ("${MMX_GIGAHORSE}" STREQUAL "TRUE")
 
-FetchContent_Declare(
-	chia_gigahorse
-	GIT_REPOSITORY https://github.com/madMAx43v3r/chia-gigahorse.git
-	GIT_TAG 4f1ddcaeb2c03d1019b4565ee7fc6c7d6878e308
-)
-FetchContent_MakeAvailable(chia_gigahorse)
+	FetchContent_Declare(
+		chia_gigahorse
+		GIT_REPOSITORY https://github.com/madMAx43v3r/chia-gigahorse.git
+		GIT_TAG 4f1ddcaeb2c03d1019b4565ee7fc6c7d6878e308
+	)
+	FetchContent_MakeAvailable(chia_gigahorse)
 
-set (GH_DESTINATION ./gigahorse/)
+	set (GH_DESTINATION ./gigahorse/)
 
-set(GH_CPU_PLOTTER_PATH ${chia_gigahorse_SOURCE_DIR}/cpu-plotter/windows)
-install(DIRECTORY ${GH_CPU_PLOTTER_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
+	set(GH_CPU_PLOTTER_PATH ${chia_gigahorse_SOURCE_DIR}/cpu-plotter/windows)
+	install(DIRECTORY ${GH_CPU_PLOTTER_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
 
-set(GH_CUDA_PLOTTER_PATH ${chia_gigahorse_SOURCE_DIR}/cuda-plotter/windows)
-install(DIRECTORY ${GH_CUDA_PLOTTER_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
+	set(GH_CUDA_PLOTTER_PATH ${chia_gigahorse_SOURCE_DIR}/cuda-plotter/windows)
+	install(DIRECTORY ${GH_CUDA_PLOTTER_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
 
-set(GH_PLOT_SINK_PATH ${chia_gigahorse_SOURCE_DIR}/plot-sink/windows)
-install(DIRECTORY ${GH_PLOT_SINK_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
+	set(GH_PLOT_SINK_PATH ${chia_gigahorse_SOURCE_DIR}/plot-sink/windows)
+	install(DIRECTORY ${GH_PLOT_SINK_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
 
-set(GH_CHIAPOS_PATH ${chia_gigahorse_SOURCE_DIR}/chiapos/windows)
-install(DIRECTORY ${GH_CHIAPOS_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
+	set(GH_CHIAPOS_PATH ${chia_gigahorse_SOURCE_DIR}/chiapos/windows)
+	install(DIRECTORY ${GH_CHIAPOS_PATH}/ DESTINATION ${GH_DESTINATION} COMPONENT plotters)
 
+endif()
 
 set(CPACK_PACKAGE_NAME "MMX Node")
 set(CPACK_PACKAGE_VENDOR "madMAx43v3r")
@@ -221,4 +229,4 @@ set(CPACK_NSIS_INSTALLED_ICON_NAME ${MMX_GUI_EXE})
 # Must be after the last CPACK macros
 include(CPack)
 
-endif()
+endif() #if (${MMX_WIN_PACK} STREQUAL "TRUE")
