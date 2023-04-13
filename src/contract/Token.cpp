9882a3ebf47f8f6484a0dd1d3c7aeeed3482e403
Max Wittal <max.wittal@mwittal.de>
2023-04-13 11:30:41 +0200

    Token::calc_hash() fix

--- src/contract/Token.cpp
+++ src/contract/Token.cpp
@@ -29,6 +29,7 @@ hash_t Token::calc_hash(const vnx::bool_t& full_hash) const
 	write_field(out, "name", 		name);
 	write_field(out, "symbol", 		symbol);
 	write_field(out, "decimals", 	decimals);
+	write_field(out, "meta_data", 	meta_data);
 	write_field(out, "owner", 		owner);
 	out.flush();
 
