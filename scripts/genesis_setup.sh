330142b3058255a666df9340c45ec45bacfe0d60
Max Wittal <max.wittal@mwittal.de>
2023-04-19 12:05:33 +0700

    token_binary + plot_nft_binary

--- scripts/genesis_setup.sh
+++ scripts/genesis_setup.sh
@@ -2,4 +2,6 @@
 
 ./build/tools/mmx_compile -f src/contract/swap.js -t -o data/tx_swap_binary.dat > config/default/chain/params/swap_binary
 ./build/tools/mmx_compile -f src/contract/offer.js -t -o data/tx_offer_binary.dat > config/default/chain/params/offer_binary
+./build/tools/mmx_compile -f src/contract/token.js -t -o data/tx_token_binary.dat > config/default/chain/params/token_binary
 ./build/tools/mmx_compile -f src/contract/virtual_plot.js -t -o data/tx_plot_binary.dat > config/default/chain/params/plot_binary
+./build/tools/mmx_compile -f src/contract/plot_nft.js -t -o data/tx_plot_nft_binary.dat > config/default/chain/params/plot_nft_binary
