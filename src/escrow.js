cec1171a11e5d1a8e9c17df3a606d798acc8d497
Max Wittal <max.wittal@mwittal.de>
2023-05-03 23:35:45 +0700

    escrow.js

---
+++ src/escrow.js
@@ -0,0 +1,29 @@
+
+var source;
+var beneficiary;
+var agent;
+
+function init(source_, beneficiary_, agent_)
+{
+	source = bech32(source_);
+	beneficiary = bech32(beneficiary_);
+	agent = bech32(agent_);
+}
+
+function deposit() public {}
+
+function unlock(currency) public
+{
+	if(this.user != agent) {
+		fail("user != agent");
+	}
+	send(beneficiary, this.balance[currency], currency);
+}
+
+function revoke(currency) public
+{
+	if(this.user != agent) {
+		fail("user != agent");
+	}
+	send(source, this.balance[currency], currency);
+}
