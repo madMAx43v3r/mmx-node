4183cc05408aab3c17cc17a799cdf3da8ed58f40
Max Wittal <max.wittal@mwittal.de>
2023-05-14 15:25:57 +0700

    removed Router sign_msg()

--- src/Router.cpp
+++ src/Router.cpp
@@ -17,8 +17,6 @@
 #include <mmx/Router_get_id_return.hxx>
 #include <mmx/Router_get_info.hxx>
 #include <mmx/Router_get_info_return.hxx>
-#include <mmx/Router_sign_msg.hxx>
-#include <mmx/Router_sign_msg_return.hxx>
 #include <mmx/Router_get_peers.hxx>
 #include <mmx/Router_get_peers_return.hxx>
 #include <mmx/Node_get_height.hxx>
@@ -1502,8 +1500,12 @@ void Router::on_return(uint64_t client, std::shared_ptr<const Return> msg)
 	{
 		case Router_get_id_return::VNX_TYPE_ID:
 			if(auto value = std::dynamic_pointer_cast<const Router_get_id_return>(result)) {
+				const auto peer = find_peer(client);
+				if(peer) {
+					peer->node_id = value->_ret_0;
+				}
 				if(value->_ret_0 == get_id()) {
-					if(auto peer = find_peer(client)) {
+					if(peer) {
 						log(INFO) << "Discovered our own address (or duplicate node ID?): " << peer->address;
 						self_addrs.insert(peer->address);
 						block_peers.insert(peer->address);
@@ -1516,18 +1518,6 @@ void Router::on_return(uint64_t client, std::shared_ptr<const Return> msg)
 			if(auto value = std::dynamic_pointer_cast<const Router_get_info_return>(result)) {
 				if(auto peer = find_peer(client)) {
 					peer->info = value->_ret_0;
-				}
-			}
-			break;
-		case Router_sign_msg_return::VNX_TYPE_ID:
-			if(auto value = std::dynamic_pointer_cast<const Router_sign_msg_return>(result)) {
-				if(auto peer = find_peer(client)) {
-					const auto& pair = value->_ret_0;
-					if(pair.second.verify(pair.first, hash_t(peer->challenge.bytes))) {
-						peer->node_id = pair.first.get_addr();
-					} else {
-						log(WARN) << "Peer " << peer->address << " failed to verify identity!";
-					}
 				}
 			}
 			break;
@@ -1685,10 +1675,6 @@ void Router::on_connect(uint64_t client, const std::string& address)
 	send_request(peer, Router_get_id::create());
 	send_request(peer, Router_get_info::create());
 	send_request(peer, Node_get_synced_height::create());
-
-	auto req = Router_sign_msg::create();
-	req->msg = peer->challenge;
-	send_request(peer, req);
 
 	if(peer_set.size() < max_peer_set) {
 		send_request(peer, Router_get_peers::create());
