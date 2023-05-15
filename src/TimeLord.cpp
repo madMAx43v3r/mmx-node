aea8917d18911fc1d138c4eb9340298af543d729
Max Wittal <max.wittal@mwittal.de>
2023-05-16 00:34:58 +0700

    recursive_sha256_ni()

--- src/TimeLord.cpp
+++ src/TimeLord.cpp
@@ -389,17 +389,13 @@ hash_t TimeLord::compute(const hash_t& input, const uint64_t num_iters)
 	static bool have_sha_ni = sha256_ni_available();
 
 	hash_t hash = input;
-	if (have_sha_ni) {
-		for (uint64_t i = 0; i < num_iters; ++i) {
-			sha256_ni(hash.data(), hash.data(), hash.size());
-		}
-	}
-	else {
-		for (uint64_t i = 0; i < num_iters; ++i) {
+	if(have_sha_ni) {
+		recursive_sha256_ni(hash.data(), num_iters);
+	} else {
+		for(uint64_t i = 0; i < num_iters; ++i) {
 			hash = hash_t(hash.bytes);
 		}
 	}
-
 	return hash;
 }
 
