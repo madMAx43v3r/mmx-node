751ddfc944731240b6978091232f76fcd25abf22
Max Wittal <max.wittal@mwittal.de>
2023-05-23 16:28:20 +0700

    more tests

--- test/vm/compiler_tests.js
+++ test/vm/compiler_tests.js
@@ -14,6 +14,10 @@ if(test1(1) != 2) {
 	fail("test1");
 }
 
+if(concat(to_string(1), to_string(2), to_string(3)) != "123") {
+	fail("concat()", 1);
+}
+
 // for(var i = 0; i < 10; ++i) {
 // 	if(i > 3) {
 // 		break;
