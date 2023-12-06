9543d456646324418b2a7cc090de8129455e7edb
Max Wittal <max.wittal@mwittal.de>
2023-12-06 21:37:05 +0700

    added HashProof

--- src/BlockHeader.cpp
+++ src/BlockHeader.cpp
@@ -49,6 +49,7 @@ std::pair<hash_t, hash_t> BlockHeader::calc_hash() const
 	write_field(out, "vdf_output", 	vdf_output);
 	write_field(out, "vdf_reward_addr", vdf_reward_addr);
 	write_field(out, "proof", 		proof ? proof->calc_hash(true) : hash_t());
+	// TODO: hash_proof
 	write_field(out, "reward_amount", reward_amount);
 	write_field(out, "reward_addr", reward_addr);
 	write_field(out, "static_cost", static_cost);
