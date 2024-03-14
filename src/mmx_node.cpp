2f4e5098915883a188f1af2b6bc73e607b1b8af0
voidxno <53347890+voidxno@users.noreply.github.com>
2024-03-14 16:38:15 +0100

    Upload, ARM SHA256 (Rev02)

--- src/mmx_node.cpp
+++ src/mmx_node.cpp
@@ -72,7 +72,9 @@ int main(int argc, char** argv)
 
 	vnx::log_info() << "AVX2 support:   " << (avx2_available() ? "yes" : "no");
 	vnx::log_info() << "SHA-NI support: " << (sha256_ni_available() ? "yes" : "no");
+#ifdef __aarch64__
 	vnx::log_info() << "ARM-SHA2 support: " << (sha256_arm_available() ? "yes" : "no");
+#endif // __aarch64__
 
 	if(with_farmer) {
 		with_wallet = true;
