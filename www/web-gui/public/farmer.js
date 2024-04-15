8e9d2b233132a0b761073194f8c6944081d246f4
Max Wittal <max.wittal@mwittal.de>
2024-04-15 19:35:20 +0700

    removed Virtual Balance

--- www/web-gui/public/farmer.js
+++ www/web-gui/public/farmer.js
@@ -57,16 +57,6 @@ Vue.component('farmer-info', {
 									</v-row>
 								</v-col>
 
-								<v-col cols="12" xl="3" md="3" sm="6" class="text-center my-2">
-									<v-row align="center" justify="space-around">
-										<div v-if="data">{{ (data.total_balance / 1e6).toFixed(2) }} MMX</div>
-										<v-skeleton-loader v-else type="heading" width="50%" align="center"></v-skeleton-loader>
-									</v-row>
-									<v-row align="center" justify="space-around" class="subtitle-1">
-										{{ $t('farmer_info.virtual_balance') }}
-									</v-row>
-								</v-col>
-
 								<v-col cols="12" xl="3" md="3" sm="6" class="text-center my-2">					
 									<v-row align="center" justify="space-around">
 										<div v-if="data">{{ (data.total_virtual_bytes / Math.pow(1000, 4)).toFixed(3) }} TBe</div>
