ce9c9e90d158de02a75aa5e2041ea7f29f59d748
Max Wittal <max.wittal@mwittal.de>
2024-04-24 23:07:03 +0700

    more database tests

--- test/test_database.cpp
+++ test/test_database.cpp
@@ -228,27 +228,43 @@ int main(int argc, char** argv)
 		mmx::Table::options_t options;
 		options.force_flush_threshold = 10;
 		auto table = std::make_shared<mmx::Table>("tmp/test_table_1", options);
-
 		table->revert(0);
 
-		const auto key = db_write(uint64_t(1337));
+		const uint32_t num_iter = 100 * options.force_flush_threshold + options.force_flush_threshold / 2;
 
-		for(uint32_t i = 0; i < 1000; ++i)
+		const auto key = db_write(uint64_t(1337));
+		const auto key2 = db_write(uint64_t(-1));
+
+		for(uint32_t i = 0; i < num_iter; ++i)
 		{
+			table->insert(key, db_write<uint32_t>(-1));
+			table->commit(i + 1);
+			table->revert(i);
+
+			if(i) {
+				vnx::test::expect(db_read<uint32_t>(table->find(key)), uint32_t(i - 1));
+			}
+			table->insert(key, db_write<uint32_t>(-1));
 			table->insert(key, db_write(i));
 			table->commit(i + 1);
 		}
+		vnx::test::expect(bool(table->find(key2)), false);
+
+		table->insert(key2, db_write<uint32_t>(-1));
+
 		table = nullptr;
 		table = std::make_shared<mmx::Table>("tmp/test_table_1", options);
 
-		for(int32_t i = 999; i >= 0; --i)
+		vnx::test::expect(bool(table->find(key2)), false);
+
+		for(int32_t i = num_iter - 1; i >= 0; --i)
 		{
 			auto value = table->find(key, i);
 			vnx::test::expect(bool(value), true);
 			vnx::test::expect(db_read<uint32_t>(value), uint32_t(i));
 		}
 
-		for(int32_t i = 1000; i >= 0; --i)
+		for(int32_t i = num_iter; i >= 0; --i)
 		{
 			table->revert(i);
 			auto value = table->find(key);
@@ -258,6 +274,66 @@ int main(int argc, char** argv)
 			} else {
 				vnx::test::expect(bool(value), false);
 			}
+		}
+	}
+	VNX_TEST_END()
+
+	VNX_TEST_BEGIN("empty_force_flush")
+	{
+		mmx::Table::options_t options;
+		options.force_flush_threshold = 10;
+		auto table = std::make_shared<mmx::Table>("tmp/empty_force_flush", options);
+		table->revert(0);
+
+		const uint32_t num_iter = 100 * options.force_flush_threshold;
+
+		for(uint32_t i = 0; i < num_iter; ++i)
+		{
+			table->commit(i + 1);
+		}
+		table = nullptr;
+		table = std::make_shared<mmx::Table>("tmp/empty_force_flush", options);
+
+		vnx::test::expect(table->current_version(), num_iter);
+	}
+	VNX_TEST_END()
+
+	VNX_TEST_BEGIN("write_log_corruption")
+	{
+		auto table = std::make_shared<mmx::Table>("tmp/write_log_corruption");
+		table->revert(0);
+
+		const uint32_t num_iter = 1000;
+		const uint32_t num_bytes = 256;
+
+		const auto key = db_write(uint64_t(1337));
+
+		for(uint32_t i = 0; i < num_iter; ++i)
+		{
+			table->insert(key, db_write(i));
+			table->commit(i + 1);
+		}
+		table = nullptr;
+		{
+			vnx::File write_log("tmp/write_log_corruption/write_log.dat");
+			write_log.open("rb+");
+			write_log.seek_to(write_log.file_size() - num_bytes);
+			std::vector<uint8_t> zeros(num_bytes);
+			write_log.out.write(zeros.data(), zeros.size());
+			write_log.close();
+		}
+		table = std::make_shared<mmx::Table>("tmp/write_log_corruption");
+
+		mmx::Table::Iterator iter(table);
+		iter.seek_begin();
+		while(iter.is_valid()) {
+			if(iter.key()->size == 0) {
+				throw std::logic_error("key size == 0");
+			}
+			if(iter.value()->size == 0) {
+				throw std::logic_error("value size == 0");
+			}
+			iter.next();
 		}
 	}
 	VNX_TEST_END()
