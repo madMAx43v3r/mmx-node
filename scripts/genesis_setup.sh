a19e589f274291b5e6eba52eac511377d014ff23
Max Wittal <max.wittal@mwittal.de>
2024-06-17 02:39:54 +0700

    setup fix

--- scripts/genesis_setup.sh
+++ scripts/genesis_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./build/tools/mmx_compile $@ -f src/contract/swap.js -t -o data/tx_swap_binary.dat > config/default/chain/params/swap_binary
-./build/tools/mmx_compile $@ -f src/contract/offer.js -t -o data/tx_offer_binary.dat > config/default/chain/params/offer_binary
-./build/tools/mmx_compile $@ -f src/contract/token.js -t -o data/tx_token_binary.dat > config/default/chain/params/token_binary
-./build/tools/mmx_compile $@ -f src/contract/virtual_plot.js -t -o data/tx_plot_binary.dat > config/default/chain/params/plot_binary
-./build/tools/mmx_compile $@ -f src/contract/plot_nft.js -t -o data/tx_plot_nft_binary.dat > config/default/chain/params/plot_nft_binary
+./build/tools/mmx_compile -n $1 -f src/contract/swap.js -t -o data/tx_swap_binary.dat > config/default/chain/params/swap_binary
+./build/tools/mmx_compile -n $1 -f src/contract/offer.js -t -o data/tx_offer_binary.dat > config/default/chain/params/offer_binary
+./build/tools/mmx_compile -n $1 -f src/contract/token.js -t -o data/tx_token_binary.dat > config/default/chain/params/token_binary
+./build/tools/mmx_compile -n $1 -f src/contract/virtual_plot.js -t -o data/tx_plot_binary.dat > config/default/chain/params/plot_binary
+./build/tools/mmx_compile -n $1 -f src/contract/plot_nft.js -t -o data/tx_plot_nft_binary.dat > config/default/chain/params/plot_nft_binary
