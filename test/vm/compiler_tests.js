10684bafb0a3213a0e2a80339a2a481967fc99df
Max Wittal <max.wittal@mwittal.de>
2024-06-22 17:47:34 +0700

    more tests

--- test/vm/compiler_tests.js
+++ test/vm/compiler_tests.js
@@ -20,4 +20,31 @@ if(concat(to_string(1), to_string(2), to_string(3)) != "123") {
 	fail("concat()", 1);
 }
 
+{
+	var map = {};
+	if(map.test != null) {
+		fail("map.test != null");
+	}
+}
+{
+	var map = {};
+	map.tmp = 123;
+	if(map.tmp != 123) {
+		fail("var to key assignment");
+	}
+}
+{
+	var map = {"test": 123};
+	const tmp = map.test;
+	if(tmp != 123) {
+		fail("key to var assignment");
+	}
+	map.tmp = map.test;
+	if(map.tmp != 123) {
+		fail("key to key assignment");
+	}
+}
 
+if(this.balance[bech32()] != 0) {
+	// fail("balance != 0");
+}
