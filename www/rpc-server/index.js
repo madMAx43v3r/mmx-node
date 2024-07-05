c7b33e98d7402f9a673adc425a65638a01f99b5c
Max Wittal <max.wittal@mwittal.de>
2024-07-05 20:10:38 +0700

    RPC redundancy + overload redirect

--- www/rpc-server/index.js
+++ www/rpc-server/index.js
@@ -1,8 +1,28 @@
 
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
+const latency_gain = 0.1;
+const check_interval_ms = 10 * 1000;
+const error_timeout_ms = 2 * check_interval_ms;
+
+const express = require('express');
+const axios = require("axios");
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+var remotes = JSON.parse(fs.readFileSync('remotes.json', 'utf8'));
+
+var targets = ['http://localhost:11380/wapi'].concat(remotes);
+
+console.log("Targets:");
+console.log(targets);
+
+var servers = [];
+for(const entry of targets) {
+	servers.push({url: entry, count: 0, errors: 0, pending: 0, latency: 1000, timeout: 0, last_response: 0});
+}
+servers[0].latency = 0;
+
+console.log(servers);
 
 var http_proxy = require('http-proxy');
 var proxy = http_proxy.createProxyServer({});
@@ -11,14 +31,80 @@ var app = express();
 
 app.all('*', function(req, res) {
 	console.log(req.method + ' ' + req.url);
+	
+	if(req.url == "/server/status") {
+		res.send(JSON.stringify(servers, null, 4) + '\n');
+		return;
+	}
+	const time_begin = Date.now();
+	
+	var back = servers[0];
+	var is_fallback = back.timeout > time_begin;
+	for(const entry of servers) {
+		if(time_begin > entry.timeout) {
+			if(entry.latency < back.latency || is_fallback) {
+				is_fallback = false;
+				back = entry;
+			}
+		}
+	}
+	back.count++;
+	back.pending++;
+	
+	res.on('finish', function() {
+		if(res.statusCode >= 500) {
+			back.errors++;
+			back.timeout = Date.now() + error_timeout_ms;
+		}
+		back.pending--;
+		
+		const now = Date.now();
+		const latency = now - time_begin;
+		back.latency = parseInt(back.latency * (1 - latency_gain) + latency * latency_gain);
+		back.last_response = now;
+		
+		console.log(req.method + ' ' + back.url + req.url + ' => status ' + res.statusCode + ', took ' + latency + ' ms, pending ' + back.pending + ', latency ' + parseInt(back.latency) + ' ms');
+    });
+	
 	proxy.web(req, res, {
-		target: 'http://localhost:11380/wapi/'
+		target: back.url
 	}, function(err) {
-		res.status(500);
-		res.send(err.code);
+		try {
+			res.status(500);
+		} finally {
+			res.send(err.code);
+		}
 		console.log(err);
 	});
 });
+
+function health_check() {
+	for(const back of servers) {
+		const time_begin = Date.now();
+		axios.get(back.url + '/node/info').then((res) =>  {
+			if(res.status >= 500) {
+				back.timeout = Date.now() + error_timeout_ms;
+			} else {
+				back.timeout = 0;
+			}
+			const now = Date.now();
+			const latency = now - time_begin;
+			if(now - back.last_response > check_interval_ms) {
+				back.latency = latency + 100;
+			} else {
+				back.latency = Math.max(back.latency, latency);
+			}
+			console.log('CHECK ' + back.url + ' => status ' + res.status + ', latency ' + latency + ' ms');
+		}).catch((err) => {
+			back.timeout = Date.now() + error_timeout_ms;
+			console.log('CHECK ' + back.url + ' failed with: ' + err.code);
+		});
+	}
+}
+
+health_check();
+
+setInterval(health_check, check_interval_ms);
 
 {
 	http.createServer(app).listen(80);
