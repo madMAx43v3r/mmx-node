e14532075709c6130052fc2a1a0ea022ac1bbb98
Max Wittal <max.wittal@mwittal.de>
2024-07-05 20:40:40 +0700

    rpc hop limit

--- www/rpc-server/index.js
+++ www/rpc-server/index.js
@@ -1,4 +1,5 @@
 
+const max_retry = 2;
 const latency_gain = 0.1;
 const check_interval_ms = 10 * 1000;
 const error_timeout_ms = 2 * check_interval_ms;
@@ -22,15 +23,16 @@ for(const entry of targets) {
 }
 servers[0].latency = 0;
 
-console.log(servers);
-
 var http_proxy = require('http-proxy');
 var proxy = http_proxy.createProxyServer({});
 
 var app = express();
 
-app.all('*', function(req, res) {
-	console.log(req.method + ' ' + req.url);
+app.all('*', function(req, res)
+{
+	const hop_count = req.get('x-hop-count');
+	
+	console.log(req.method + ' ' + req.url + ' (hop ' + (hop_count ? hop_count : 0) + ')');
 	
 	if(req.url == "/server/status") {
 		res.send(JSON.stringify(servers, null, 4) + '\n');
@@ -39,12 +41,14 @@ app.all('*', function(req, res) {
 	const time_begin = Date.now();
 	
 	var back = servers[0];
-	var is_fallback = back.timeout > time_begin;
-	for(const entry of servers) {
-		if(time_begin > entry.timeout) {
-			if(entry.latency < back.latency || is_fallback) {
-				is_fallback = false;
-				back = entry;
+	if(!hop_count || hop_count <= max_retry) {
+		var is_fallback = back.timeout > time_begin;
+		for(const entry of servers) {
+			if(time_begin > entry.timeout) {
+				if(entry.latency < back.latency || is_fallback) {
+					is_fallback = false;
+					back = entry;
+				}
 			}
 		}
 	}
@@ -65,9 +69,10 @@ app.all('*', function(req, res) {
 		
 		console.log(req.method + ' ' + back.url + req.url + ' => status ' + res.statusCode + ', took ' + latency + ' ms, pending ' + back.pending + ', latency ' + parseInt(back.latency) + ' ms');
     });
-	
+    
 	proxy.web(req, res, {
-		target: back.url
+		target: back.url,
+		headers: {'x-hop-count': hop_count + 1}
 	}, function(err) {
 		try {
 			res.status(500);
@@ -82,9 +87,13 @@ function health_check() {
 	for(const back of servers) {
 		const time_begin = Date.now();
 		axios.get(back.url + '/node/info').then((res) =>  {
+			var info = {};
 			if(res.status >= 500) {
 				back.timeout = Date.now() + error_timeout_ms;
 			} else {
+				if(res.data) {
+					info = res.data;
+				}
 				back.timeout = 0;
 			}
 			const now = Date.now();
@@ -94,7 +103,7 @@ function health_check() {
 			} else {
 				back.latency = Math.max(back.latency, latency);
 			}
-			console.log('CHECK ' + back.url + ' => status ' + res.status + ', latency ' + latency + ' ms');
+			console.log('CHECK ' + back.url + ' => status ' + res.status + ', height ' + info.height + ', latency ' + latency + ' ms');
 		}).catch((err) => {
 			back.timeout = Date.now() + error_timeout_ms;
 			console.log('CHECK ' + back.url + ' failed with: ' + err.code);
