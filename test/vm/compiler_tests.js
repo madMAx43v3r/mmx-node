fbeb3b7fac3735b07a2ba787d792905335dcc056
Max Wittal <max.wittal@mwittal.de>
2024-07-05 15:36:23 +0700

    more tests

--- test/vm/compiler_tests.js
+++ test/vm/compiler_tests.js
@@ -48,3 +48,88 @@ if(concat(to_string(1), to_string(2), to_string(3)) != "123") {
 if(this.balance[bech32()] != 0) {
 	// fail("balance != 0");
 }
+
+{
+	var array = [1, 2, 3];
+	for(var entry of array) {
+		entry = 0;
+	}
+	for(const entry of array) {
+		if(entry == 0) {
+			fail("var entry of array (integer)");
+		}
+	}
+}
+{
+	var array = ["1", "2", "3"];
+	for(var entry of array) {
+		entry = "0";
+	}
+	for(const entry of array) {
+		if(entry == "0") {
+			fail("var entry of array (string)");
+		}
+	}
+}
+{
+	var array = [[1], [2], [3]];
+	for(var entry of array) {
+		entry = [];
+	}
+	for(const entry of array) {
+		if(size(entry) == 0) {
+			fail("var entry of array (array)");
+		}
+	}
+}
+{
+	var array = [{val: 1}, {val: 2}, {val: 3}];
+	for(var entry of array) {
+		entry = {val: 0};
+	}
+	for(const entry of array) {
+		if(entry.val == 0) {
+			fail("var entry of array (object)");
+		}
+	}
+}
+
+{
+	var obj = {key: "value"};
+	if(obj.key != "value") {
+		fail("object key (1)");
+	}
+}
+{
+	var obj = {"key": "value"};
+	if(obj.key != "value") {
+		fail("object key (2)");
+	}
+}
+{
+	var obj = {test: 123, key: "value"};
+	if(obj.key != "value") {
+		fail("object key (3)");
+	}
+}
+{
+	var obj = { key : "value" };
+	if(obj.key != "value") {
+		fail("object key (4)");
+	}
+}
+{
+	var obj = { 	key		 : 	"value" };
+	if(obj.key != "value") {
+		fail("object key (5)");
+	}
+}
+{
+	var obj = {_key_: "value"};
+	if(obj._key_ != "value") {
+		fail("object _key_");
+	}
+}
+
+
+
