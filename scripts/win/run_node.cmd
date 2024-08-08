4ae0ac4af52ed165019dd6ecd84649ca9b2365c1
Max Wittal <max.wittal@mwittal.de>
2024-08-08 17:35:49 +0700

    windows cmd absolte config paths for all

--- scripts/win/run_node.cmd
+++ scripts/win/run_node.cmd
@@ -3,6 +3,6 @@ TITLE MMX Node Run Script
 
 CALL .\activate.cmd
 
-mmx_node -c config\%NETWORK%\ config\node\ "%MMX_HOME%config\local\\" %*
+mmx_node -c "%MMX_HOME%config\%NETWORK%\" "%MMX_HOME%config\node\" "%MMX_HOME%config\local\" %*
 
 IF "%~1"=="--PauseOnExit" PAUSE
--- scripts/win/run_wallet.cmd
+++ scripts/win/run_wallet.cmd
@@ -3,4 +3,4 @@ TITLE MMX Wallet Run Script
 
 CALL .\activate.cmd
 
-mmx_wallet -c config\%NETWORK%\ config\wallet\ "%MMX_HOME%config\local\\" %*
+mmx_wallet -c "%MMX_HOME%config\%NETWORK%\" "%MMX_HOME%config\wallet\" "%MMX_HOME%config\local\" %*
