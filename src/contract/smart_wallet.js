d73fea3623f8a232ef86717ec71ed6d7a2c88293
Max Wittal <max.wittal@mwittal.de>
2024-11-02 04:07:58 +0700

    smart_wallet.js

---
+++ src/contract/smart_wallet.js
@@ -0,0 +1,74 @@
+
+var owner;
+var plans = {};
+
+function init(owner_)
+{
+	owner = bech32(owner_);
+}
+
+function check_owner()
+{
+	if(this.user != owner) {
+		fail("user != owner", 1);
+	}
+}
+
+function add_plan(name, amount, currency, target, memo, interval, start) public
+{
+	check_owner();
+	
+	const prev = plans[name];
+	if(prev) {
+		if(prev.active) {
+			fail("plan already exists", 2);
+		}
+	}
+	if(memo != null) {
+		memo = to_string(memo);
+	}
+	if(start == null) {
+		start = this.height;
+	}
+	const plan = {
+		name: name,
+		amount: amount,
+		currency: bech32(currency),
+		target: bech32(target),
+		memo: memo,
+		interval: interval,
+		next_pay: start,
+		active: true
+	};
+	plans[name] = plan;
+}
+
+function cancel_plan(name) public
+{
+	check_owner();
+	
+	const plan = plans[name];
+	if(!plan) {
+		fail("no such plan", 3);
+	}
+	plan.active = false;
+}
+
+function plan_payment(name) public
+{
+	const plan = plans[name];
+	if(!plan) {
+		fail("no such plan", 3);
+	}
+	if(!plan.active) {
+		fail("plan has been canceled", 4);
+	}
+	if(this.height < plan.next_pay) {
+		fail("payment too soon", 5);
+	}
+	plan.next_pay += plan.interval;
+	
+	send(plan.target, plan.amount, plan.currency, plan.memo);
+}
+
+
