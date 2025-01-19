a44c240c50fc037e73ecc9321336b75b45391a89
voidxno <53347890+voidxno@users.noreply.github.com>
2025-01-19 21:10:55 +0100

    Upload, Version Tuning (Rev02, feedback)

--- cmake/build_version_json.cmake
+++ cmake/build_version_json.cmake
@@ -26,13 +26,13 @@ execute_process(
 # NOTE: 'version' / only accept 'v0.13.9' or 'v0.13.9-8-gd7d82d79' format (max 80 chars), else fallback to 'v0.0.0.20250117'
 # NOTE: 'commit' / only accept SHA1 format (lowercase, 40 chars), else fallback to '0000000000000000000000000000000000000000'
 
-string(LENGTH ${GIT_BUILD_VERSION} GIT_BUILD_VERSION_LEN)
-if(NOT GIT_BUILD_VERSION MATCHES "(^v([0-9]+)\\.([0-9]+)\\.([0-9]+)$)|(^v([0-9]+)\\.([0-9]+)\\.([0-9]+)\\-([0-9]+)\\-g([0-9a-f]+)$)" OR GIT_BUILD_VERSION_LEN GREATER 80)
+string(LENGTH "${GIT_BUILD_VERSION}" GIT_BUILD_VERSION_LEN)
+if(NOT GIT_BUILD_VERSION MATCHES "^v[0-9]+\\.[0-9]+\\.[0-9]+$|^v[0-9]+\\.[0-9]+\\.[0-9]+\\-[0-9]+\\-g[0-9a-f]+$" OR GIT_BUILD_VERSION_LEN GREATER 80)
   string(TIMESTAMP GIT_BUILD_TIMESTAMP "%Y%m%d")
   set(GIT_BUILD_VERSION "v0.0.0.${GIT_BUILD_TIMESTAMP}")
 endif()
 
-string(LENGTH ${GIT_BUILD_COMMIT} GIT_BUILD_COMMIT_LEN)
+string(LENGTH "${GIT_BUILD_COMMIT}" GIT_BUILD_COMMIT_LEN)
 if(NOT GIT_BUILD_COMMIT MATCHES "^[0-9a-f]+$" OR NOT GIT_BUILD_COMMIT_LEN EQUAL 40)
   set(GIT_BUILD_COMMIT "0000000000000000000000000000000000000000")
 endif()
