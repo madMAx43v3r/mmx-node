c52ec986680ff496c895b6ae38e0e2622f9d5eb8
Max Wittal <max.wittal@mwittal.de>
2025-01-25 20:33:11 +0700

    wallet gui exit if input canceled

--- run_wallet_gui.sh
+++ run_wallet_gui.sh
@@ -6,6 +6,10 @@ NODE=$(cat "${MMX_HOME}config/local/node")
 
 NODE=$(zenity --entry --title="Remote Node Address" --text="Node IP or hostname:" --entry-text="$NODE")
 
+if [[ -z "${NODE}" ]]; then
+	exit -1
+fi
+
 echo "$NODE" > "${MMX_HOME}config/local/node"
 
 mmx_wallet -c config/${NETWORK}/ config/wallet/ "${MMX_HOME}config/local/" --gui $@
