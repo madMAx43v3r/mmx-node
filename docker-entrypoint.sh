9d5a4f264af491e28dc652844e790214362c93ec
matt <ace@worx.wondermining.net>
2025-02-03 22:47:54 -0600

    add environment variables for docker remote services

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-set -em
 
-source ./activate.sh
+config_path="${MMX_HOME}config/local/"
 
-"$@"
+if [ -d "$config_path" ]; then
+
+	if [[ "${MMX_ALLOW_REMOTE}" == "true" ]]; then
+	  echo true > "${config_path}allow_remote" && echo "*** Remote Access Enabled ***"
+	elif [[ "${MMX_ALLOW_REMOTE}" == "false" ]]; then
+	  echo false > "${config_path}allow_remote" && echo "*** Remote Access Disabled ***"
+	fi
+
+	if [[ "${MMX_HARVESTER_ENABLED}" == "true" ]]; then
+	  echo true > "${config_path}harvester" && echo "*** Harvester Enabled ***"
+	elif [[ "${MMX_HARVESTER_ENABLED}" == "false" ]]; then
+	  echo false > "${config_path}harvester" && echo "*** Harvester Disabled ***"
+	fi
+
+	if [[ "${MMX_FARMER_ENABLED}" == "true" ]]; then
+	  echo true > "${config_path}farmer" && echo "*** Farmer Enabled ***"
+	elif [[ "${MMX_FARMER_ENABLED}" == "false" ]]; then
+	  echo false > "${config_path}farmer" && echo "*** Farmer Disabled ***"
+	fi
+
+fi
+
+exec "$@"
