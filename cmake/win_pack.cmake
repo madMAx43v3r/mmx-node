e878ce8ad71fefe4d2165cc7d36a859e9286e561
stotiks <47013208+stotiks@users.noreply.github.com>
2025-02-05 15:26:28 +0200

    win: cuda

--- cmake/win_pack.cmake
+++ cmake/win_pack.cmake
@@ -70,7 +70,7 @@ set(MMX_BUNDLE "MMX Node")
 list(APPEND APP_FILES
 	mmx mmx_node mmx_farmer mmx_wallet mmx_timelord mmx_harvester
 	mmx_compile mmx_postool
-	mmx_db mmx_vm mmx_iface mmx_modules mmx_pos
+	mmx_iface mmx_modules
 	vnx_base vnx_addons url_cpp llhttp
 	vnxpasswd generate_passwd
 	automy_basic_opencl
@@ -134,6 +134,7 @@ install(FILES
 			$<TARGET_FILE_DIR:vnx_addons>/zstd.dll
 			$<TARGET_FILE_DIR:automy_basic_opencl>/OpenCL.dll
 			$<TARGET_FILE_DIR:mmx_modules>/miniupnpc.dll
+			$<TARGET_FILE_DIR:mmx_modules>/cudart64_12.dll
 		DESTINATION ./ COMPONENT applications)
 
 install(DIRECTORY kernel/ DESTINATION kernel COMPONENT applications)
