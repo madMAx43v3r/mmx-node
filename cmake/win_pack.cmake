ddbd1590592848176fc1ba94a285d45c1f537142
stotiks <47013208+stotiks@users.noreply.github.com>
2025-02-05 20:47:21 +0200

    win: cpack mmx_posbench

--- cmake/win_pack.cmake
+++ cmake/win_pack.cmake
@@ -69,7 +69,7 @@ set(MMX_BUNDLE "MMX Node")
 
 list(APPEND APP_FILES
 	mmx mmx_node mmx_farmer mmx_wallet mmx_timelord mmx_harvester
-	mmx_compile mmx_postool
+	mmx_compile mmx_postool mmx_posbench
 	mmx_iface mmx_modules
 	vnx_base vnx_addons url_cpp llhttp
 	vnxpasswd generate_passwd
