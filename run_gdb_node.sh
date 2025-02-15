cb8fa23766bc76352554879c112360b25a6251ea
Max Wittal <max.wittal@mwittal.de>
2025-02-15 15:38:43 +0700

    disable kill_high_mem.sh for now

--- run_gdb_node.sh
+++ run_gdb_node.sh
@@ -2,7 +2,7 @@
 
 source ./activate.sh
 
-./scripts/kill_high_mem.sh mmx_node 8192 &
+#./scripts/kill_high_mem.sh mmx_node 8192 &
 
 gdb -ex=run --args ./build/mmx_node -c config/${NETWORK}/ config/node/ "${MMX_HOME}config/local/" $@
 
