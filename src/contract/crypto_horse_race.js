59f902ce96b5e3103720b784ce2c9f832579352e
Max Wittal <max.wittal@mwittal.de>
2025-03-22 14:40:35 +0700

    crypto_horse_race.js

---
+++ src/contract/crypto_horse_race.js
@@ -0,0 +1,112 @@
+
+const FRACT_BITS = 64;
+const PRICE_ITERS = 20;
+
+var currency;
+var PRECISION = 1;			// price increment
+var TOKEN_SCALE = 1;
+
+var wallets = {};			// user token balances
+var balance = [];			// currency per horse
+
+function init(num_horses, currency_, precision, decimals = 9)
+{
+	for(var i = 0; i < num_horses; ++i) {
+		push(balance, 0);
+	}
+	currency = bech32(currency_);
+	
+	for(var i = 0; i < precision; ++i) {
+		PRECISION *= 10;
+	}
+	for(var i = 0; i < decimals; ++i) {
+		TOKEN_SCALE *= 10;
+	}
+}
+
+function calc_price(x) const public
+{
+	x /= PRECISION;
+	var y = x;
+	for(var i = 0; i < PRICE_ITERS; ++i) {
+		y = ((y << FRACT_BITS) + (x << FRACT_BITS) / y) >> (FRACT_BITS + 1);
+	}
+	return max(y, 100) * PRECISION;
+}
+
+function get_price(index) const public
+{
+	return calc_price(balance[index]);
+}
+
+function get_buy_price(index, amount) const public
+{
+	return calc_price(balance[index] + uint(amount));
+}
+
+function get_sell_price(index, amount) const public
+{
+	return calc_price(balance[index] - ((uint(amount) * get_price(index)) / TOKEN_SCALE));
+}
+
+function get_wallet(user)
+{
+	var wallet = wallets[user];
+	if(!wallet) {
+		wallet = {};
+		wallets[user] = wallet;
+	}
+	return wallet;
+}
+
+function get_wallet_balance(wallet, index)
+{
+	var balance = wallet[index];
+	if(!balance) {
+		balance = 0;
+	}
+	return balance;
+}
+
+function buy_horse(index) public payable
+{
+	assert(index < size(balance), "no such horse");
+	assert(this.deposit.currency == currency, "wrong currency");
+	
+	const price = get_buy_price(index, this.deposit.amount);
+	const token_amount = (this.deposit.amount * TOKEN_SCALE) / price;
+	
+	const wallet = get_wallet(this.user);
+	wallet[index] = get_wallet_balance(wallet, index) + token_amount;
+	
+	balance[index] += this.deposit.amount;
+}
+
+function sell_horse(index, amount) public
+{
+	amount = uint(amount);
+	
+	assert(amount > 0);
+	assert(index < size(balance), "no such horse");
+	
+	const wallet = get_wallet(this.user);
+	const wallet_balance = get_wallet_balance(wallet, index);
+	
+	assert(amount <= wallet_balance, "insufficient balance");
+	
+	const price = get_sell_price(index, amount);
+	const ret_amount = (amount * price) / TOKEN_SCALE;
+	
+	wallet[index] = wallet_balance - amount;
+	balance[index] -= ret_amount;
+	
+	send(this.user, ret_amount, currency);
+}
+
+
+
+
+
+
+
+
