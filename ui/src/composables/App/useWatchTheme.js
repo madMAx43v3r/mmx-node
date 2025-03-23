104ca2df5e5c916410990209aa69d1a8c42aaf88
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-23 19:54:35 +0200

    Remove console log from theme watcher

--- ui/src/composables/App/useWatchTheme.js
+++ ui/src/composables/App/useWatchTheme.js
@@ -6,7 +6,6 @@ export const useWatchTheme = () => {
     watchEffect(() => $q.dark.set(appStore.isDarkTheme));
 
     if (window.mmx?.theme_dark !== undefined && !interval) {
-        console.log(window.mmx.theme_dark, interval);
         interval = setInterval(() => {
             appStore.isDarkTheme = window.mmx.theme_dark;
         }, 500);
