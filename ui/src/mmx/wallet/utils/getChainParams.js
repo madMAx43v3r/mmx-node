235018e24544780b1c76c909d99acb827cd2ff73
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-23 18:22:05 +0200

    mmxConfig alias getChainParams adjustments

--- ui/src/mmx/wallet/utils/getChainParams.js
+++ ui/src/mmx/wallet/utils/getChainParams.js
@@ -1,6 +1,7 @@
-const chainParamsList = import.meta.glob("../../config/(mainnet|mainnet-rc|testnet??)/chain/params.json");
+const chainParamsList = import.meta.glob("@mmxConfig/(mainnet|mainnet-rc|testnet??)/chain/params.json");
 
 export const getChainParams = async (network) => {
-    const chainParams = await chainParamsList[`../../config/${network}/chain/params.json`]();
+    const path = Object.keys(chainParamsList).filter((key) => key.endsWith(`${network}/chain/params.json`));
+    const chainParams = await chainParamsList[path]();
     return chainParams;
 };
