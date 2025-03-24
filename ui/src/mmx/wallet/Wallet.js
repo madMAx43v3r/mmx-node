d835d6f256fd8a52cbf8be4ced5bf43d305d9336
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-24 03:07:49 +0200

    Add getSendManyTxAsync method and corresponding tests in Wallet module

--- ui/src/mmx/wallet/Wallet.js
+++ ui/src/mmx/wallet/Wallet.js
@@ -8,5 +8,15 @@ class Wallet {
         await ecdsaWallet.completeAsync(tx, options);
         return tx;
     }
+
+    static async getSendManyTxAsync(ecdsaWallet, payments, dst_addr, currency, options) {
+        const tx = new Transaction();
+        tx.note = "TRANSFER";
+        payments.forEach((payment) => {
+            tx.add_output(currency, payment.address, payment.amount, options.memo);
+        });
+        await ecdsaWallet.completeAsync(tx, options);
+        return tx;
+    }
 }
 export { Wallet };
--- ui/src/mmx/wallet/Wallet.js.test.js
+++ ui/src/mmx/wallet/Wallet.js.test.js
@@ -4,6 +4,8 @@ import { ECDSA_Wallet } from "./ECDSA_Wallet";
 import { addr_t } from "./common/addr_t";
 import { Wallet } from "./Wallet";
 
+import "./Transaction.ext";
+
 const mnemonic = import.meta.env.VITE_TEST_MNEMONIC;
 const ecdsaWallet = new ECDSA_Wallet(mnemonic, "");
 
@@ -23,5 +25,19 @@ describe("Wallet", () => {
     it("getSendTxAsync", async () => {
         const tx = await Wallet.getSendTxAsync(ecdsaWallet, amount, dst_addr, currency, options);
         assert.equal(tx.id, "4EEC4FF00DE1ED2138384B9FD6B9116F86897BF4CADDDF2BA071231B9520EAE9");
+    });
+
+    it("getSendManyTxAsync", async () => {
+        const tx = await Wallet.getSendManyTxAsync(
+            ecdsaWallet,
+            [
+                { address: "mmx16aq5vpcmxcrh9xck0z06eqnmr87w5r2j062snjj6g7cvj0thry7q0mp3w6", amount: 1 },
+                { address: "mmx1mw38rg8jcy2tjc5r7sxque6z45qrw6dsu6g2wmhahwf30342rraqyhsnea", amount: 1 },
+            ],
+            dst_addr,
+            currency,
+            options
+        );
+        assert.equal(tx.id, "CB60F7A2B26FB3FEFD26F78514C7AD5C5B22BCD50FD785EDFB09937AAC41C8DB");
     });
 });
