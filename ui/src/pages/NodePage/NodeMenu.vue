521207966177ecf2b80c8627014a531e4fb37d2f
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-25 01:50:56 +0200

    Update NodeMenu to use 'visible' property for tab visibility instead of 'hide'

--- ui/src/pages/NodePage/NodeMenu.vue
+++ ui/src/pages/NodePage/NodeMenu.vue
@@ -2,7 +2,7 @@
     <q-tabs align="left" indicator-color="primary" outside-arrows class="m-bg-grey">
         <template v-for="(item, key) in tabs" :key="key">
             <q-route-tab
-                v-if="!item.hide"
+                v-if="item.visible ?? true"
                 :to="item.to"
                 :exact="item.exact ?? false"
                 :label="item.label"
@@ -18,7 +18,7 @@ const { isLocalNode } = useConfigData();
 const { t } = useI18n();
 const tabs = computed(() => [
     { to: "/node/log", label: t("node_menu.log") },
-    { to: "/node/peers", label: t("node_menu.peers"), hide: !isLocalNode.value },
+    { to: "/node/peers", label: t("node_menu.peers"), visible: isLocalNode.value },
     { to: "/node/blocks", label: t("node_menu.blocks") },
     { to: "/node/netspace", label: t("node_menu.netspace") },
     { to: "/node/vdf_speed", label: t("node_menu.vdf_speed") },
