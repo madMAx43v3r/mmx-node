d82d32772bf62acbd1fd38b26113e41784516068
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-25 02:20:31 +0200

    Add AccountContractView component and refactor AccountContractsIndex to use it

---
+++ ui/src/pages/Wallet/Account/AccountContracts/AccountContractView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="row q-gutter-x-none">
+            <m-chip>{{ data.__type }}</m-chip>
+            <m-chip>{{ data.address }}</m-chip>
+        </div>
+
+        <q-card flat>
+            <ObjectTable :data="data" />
+        </q-card>
+    </div>
+    <div>
+        <BalanceTable :address="data.address" />
+    </div>
+    <div v-if="data.__type != 'mmx.contract.Executable'" class="row justify-end q-gutter-xs">
+        <q-btn
+            :label="$t('account_contract_summary.deposit')"
+            :icon="mdiBankTransferIn"
+            outline
+            color="positive"
+            @click="handleDeposit(index, data.address)"
+        />
+
+        <q-btn
+            :label="$t('account_contract_summary.withdraw')"
+            :icon="mdiBankTransferOut"
+            outline
+            color="negative"
+            @click="handleWithdraw(index, data.address)"
+        />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
--- ui/src/pages/Wallet/Account/AccountContracts/AccountContractsIndex.vue
+++ ui/src/pages/Wallet/Account/AccountContracts/AccountContractsIndex.vue
@@ -2,40 +2,13 @@
     <!-- TODO filter -->
 
     <div v-for="(row, key) in filteredRows" :key="key" class="q-gutter-y-xs">
-        <div>
-            <div class="row q-gutter-x-none">
-                <m-chip>{{ row.__type }}</m-chip>
-                <m-chip>{{ row.address }}</m-chip>
-            </div>
-
-            <q-card flat>
-                <ObjectTable :data="row" />
-            </q-card>
-        </div>
-        <div>
-            <BalanceTable :address="row.address" />
-        </div>
-        <div v-if="row.__type != 'mmx.contract.Executable'" class="row justify-end q-gutter-xs">
-            <q-btn
-                :label="$t('account_contract_summary.deposit')"
-                :icon="mdiBankTransferIn"
-                outline
-                color="positive"
-                @click="handleDeposit(index, row.address)"
-            />
-
-            <q-btn
-                :label="$t('account_contract_summary.withdraw')"
-                :icon="mdiBankTransferOut"
-                outline
-                color="negative"
-                @click="handleWithdraw(index, row.address)"
-            />
-        </div>
+        <AccountContractView :data="row" />
     </div>
 </template>
 <script setup>
+import AccountContractView from "./AccountContractView";
 import { mdiBankTransferIn, mdiBankTransferOut } from "@mdi/js";
+
 const props = defineProps({
     index: {
         type: Number,
