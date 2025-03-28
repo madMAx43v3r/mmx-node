1be3d6227aa2246afa9d6dd77b0594d0fb89535b
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-29 01:47:47 +0200

    feat: add ChainParams class for managing blockchain parameters and update getChainParams to utilize it

--- ui/src/mmx/wallet/utils/getChainParams.js
+++ ui/src/mmx/wallet/utils/getChainParams.js
@@ -1,7 +1,10 @@
+import { ChainParams } from "./ChainParams";
+
 const chainParamsList = import.meta.glob("@mmxConfig/(mainnet|mainnet-rc|testnet??)/chain/params.json");
 
 export const getChainParams = async (network) => {
     const path = Object.keys(chainParamsList).filter((key) => key.endsWith(`${network}/chain/params.json`));
-    const chainParams = await chainParamsList[path]();
+    const _chainParams = await chainParamsList[path]();
+    const chainParams = new ChainParams(_chainParams);
     return chainParams;
 };
