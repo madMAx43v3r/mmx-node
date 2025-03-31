d53a797f8b0b1ae7f939ccab5b5fd38e2b097216
stotiks <47013208+stotiks@users.noreply.github.com>
2025-03-31 23:27:06 +0300

    feat: enhance getChainParams to include additional chain parameters from separate files

--- ui/src/mmx/wallet/utils/getChainParams.js
+++ ui/src/mmx/wallet/utils/getChainParams.js
@@ -1,10 +1,24 @@
 import { ChainParams } from "./ChainParams";
 
-const chainParamsList = import.meta.glob("@mmxConfig/(mainnet|mainnet-rc|testnet??)/chain/params.json");
+const chainParamsList = import.meta.glob(`@mmxConfig/(mainnet|mainnet-rc|testnet??)/chain/params.json`);
+const chainExtraParamsList = import.meta.glob(`@mmxConfig/(mainnet|mainnet-rc|testnet??)/chain/params/(*)`, {
+    as: "raw",
+});
 
 export const getChainParams = async (network) => {
+    // get chain params
     const path = Object.keys(chainParamsList).filter((key) => key.endsWith(`${network}/chain/params.json`));
     const _chainParams = await chainParamsList[path]();
+
+    // get chain extra params
+    const extraPath = Object.keys(chainExtraParamsList).filter((key) => key.includes(`${network}/chain/params/`));
+
+    for (const key of extraPath) {
+        const param = key.match(/\/chain\/params\/(.*)$/)[1];
+        const extraParam = (await chainExtraParamsList[key]()).trim();
+        _chainParams[param] = extraParam;
+    }
+
     const chainParams = new ChainParams(_chainParams);
     return chainParams;
 };
