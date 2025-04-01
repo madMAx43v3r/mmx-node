f8134ff89cdc4dd06def18813e7815fdfd2b65db
Max Wittal <max.wittal@mwittal.de>
2025-04-01 22:19:25 +0700

    transfer function + activation height

--- src/contract/crypto_horse_race.js
+++ src/contract/crypto_horse_race.js
@@ -1,20 +1,24 @@
 
 const FRACT_BITS = 64;
 const PRICE_ITERS = 20;
+const TRANSFER_EXPIRE = 360;	// blocks
 
 var currency;
-var PRECISION = 1;			// price increment
+var PRECISION = 1;				// price increment
 var TOKEN_SCALE = 1;
+var activation_height;
 
-var wallets = {};			// user token balances
-var balance = [];			// currency per horse
+var wallets = {};				// user token balances
+var balance = [];				// currency per horse
 
-function init(num_horses, currency_, precision, decimals = 9)
+
+function init(num_horses, currency_, precision, decimals, activation_height_)
 {
 	for(var i = 0; i < num_horses; ++i) {
 		push(balance, 0);
 	}
 	currency = bech32(currency_);
+	activation_height = uint(activation_height_);
 	
 	for(var i = 0; i < precision; ++i) {
 		PRECISION *= 10;
@@ -51,6 +55,8 @@ function get_sell_price(index, amount) const public
 
 function get_wallet(user)
 {
+	assert(user, "invalid user");
+	
 	var wallet = wallets[user];
 	if(!wallet) {
 		wallet = {};
@@ -71,6 +77,7 @@ function get_wallet_balance(wallet, index)
 function buy_horse(index) public payable
 {
 	assert(index < size(balance), "no such horse");
+	assert(this.height >= activation_height, "not active yet");
 	assert(this.deposit.currency == currency, "wrong currency");
 	
 	const price = get_buy_price(index, this.deposit.amount);
@@ -103,8 +110,26 @@ function sell_horse(index, amount) public
 	send(this.user, ret_amount, currency);
 }
 
-
-
+function transfer(target, amount, index) public
+{
+	target = bech32(target);
+	amount = uint(amount);
+	
+	assert(amount > 0);
+	assert(index < size(balance), "no such horse");
+	
+	const wallet = get_wallet(this.user);
+	const wallet_balance = get_wallet_balance(wallet, index);
+	
+	assert(amount <= wallet_balance, "insufficient balance");
+	
+	const target_wallet = get_wallet(target);
+	
+	assert(target_wallet, "no such wallet");
+	
+	wallet[index] -= amount;
+	target_wallet[index] = get_wallet_balance(target_wallet, index) + amount;
+}
 
 
 
