d5dbedc78ae15b2dfbcc3bdf8f128a9c64ea0f66
Max Wittal <max.wittal@mwittal.de>
2025-04-02 22:49:13 +0700

    added compare()

---
+++ src/vm/std/compare.js
@@ -0,0 +1,28 @@
+// Returns "EQ" if arguments are equal, "LT" if L < R and "GT" if L > R
+// Note: Arrays are (recursively) deep compared, otherwise the same as language operators
+function compare(L, R) {
+    if(!is_array(L) || !is_array(R)) {
+        if(L < R) {
+            return "LT";
+        }
+        if(L > R) {
+            return "GT";
+        }
+        return "EQ";
+    }
+    const N = size(L);
+    const M = size(R);
+    if(N != M) {
+        if(N < M) {
+            return "LT";
+        }
+        return "GT";
+    }
+    for(var i = 0; i < N; ++i) {
+        const res = compare(L[i], R[i]);
+        if(res != "EQ") {
+            return res;
+        }
+    }
+    return "EQ";
+}
