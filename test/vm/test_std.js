bb99a19700cd2e654cc7d9911c4cf1cd0cfa18fa
Max Wittal <max.wittal@mwittal.de>
2025-04-02 22:49:40 +0700

    compare() tests

--- test/vm/test_std.js
+++ test/vm/test_std.js
@@ -1,4 +1,4 @@
-import {equals, sort, reverse} from "std";
+import {equals, sort, reverse, compare} from "std";
 
 function main() {
 
@@ -24,6 +24,19 @@ assert(equals(reverse([]), []));
 assert(equals(reverse([1]), [1]));
 assert(equals(reverse([1, 2]), [2, 1]));
 assert(equals(reverse([1, 2, 3]), [3, 2, 1]));
+
+assert(compare(0, 1) == "LT");
+assert(compare(1, 0) == "GT");
+assert(compare(1, 1) == "EQ");
+assert(compare([], []) == "EQ");
+assert(compare([0], [0]) == "EQ");
+assert(compare([1, 2], [1, 2]) == "EQ");
+assert(compare([1, 2], [1, 3]) == "LT");
+assert(compare([0, 2], [1, 2]) == "LT");
+assert(compare([1, 3], [1, 2]) == "GT");
+assert(compare([2, 2], [1, 2]) == "GT");
+assert(compare([0, 0], [1, 0]) == "LT");
+assert(compare([0, 1], [0, 0]) == "GT");
 
 } // main
 
