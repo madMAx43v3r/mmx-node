1cbe225124e9d615ac1d030549efc82e8b847fe8
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-07 23:38:03 +0300

    refactor: simplify tx.vue by removing unused wallet and address logic

--- ui/src/pages/!tests/tx.vue
+++ ui/src/pages/!tests/tx.vue
@@ -1,44 +1,16 @@
 <template>
     <q-page padding>
         <div class="q-gutter-y-sm">
-            <template v-if="!wallet">
-                <SeedInputForm v-model:wallet="wallet" />
-            </template>
-            <template v-else-if="address">
-                <div>
-                    <div class="row q-gutter-x-none">
-                        <m-chip>{{ $t("common.address") }}</m-chip>
-                        <m-chip>{{ address }}</m-chip>
-                        <div class="col q-my-auto">
-                            <q-btn
-                                label="Logout"
-                                :icon="mdiLogout"
-                                size="sm"
-                                class="float-right"
-                                @click="handleLogout"
-                            />
-                        </div>
-                    </div>
+            <q-input v-model="contract" filled type="textarea" input-style="height: 500px" />
+            <div class="q-gutter-x-xs">
+                <q-btn label="Format" color="secondary" @click="handleFormat" />
+                <q-btn label="Validate" color="primary" @click="handleValidate" />
+                <q-btn label="Broadcast" color="negative" @click="handleBroadcast" />
+            </div>
 
-                    <q-card flat>
-                        <BalanceTable :address="address" show-empty />
-                    </q-card>
-                </div>
-                <div class="q-gutter-y-sm">
-                    <q-input v-model="contract" filled type="textarea" input-style="height: 500px" />
-                    <div class="q-gutter-x-xs">
-                        <q-btn label="Validate" color="primary" @click="handleValidate" />
-                        <q-btn label="Broadcast" color="primary" @click="handleBroadcast" />
-                    </div>
-
-                    <q-inner-loading
-                        :showing="transactionValidate.isPending.value || transactionBroadcast.isPending.value"
-                    >
-                        <q-spinner-gears color="primary" size="3em" />
-                    </q-inner-loading>
-                </div>
-                <AddressHistoryTable :address="address" :limit="20" show-empty />
-            </template>
+            <q-inner-loading :showing="transactionValidate.isPending.value || transactionBroadcast.isPending.value">
+                <q-spinner-gears color="primary" size="3em" />
+            </q-inner-loading>
         </div>
     </q-page>
 </template>
@@ -60,43 +32,22 @@ const address = computedAsync(
     }
 );
 
-const init = {
-    note: "TRANSFER",
-    sender: "mmx16aq5vpcmxcrh9xck0z06eqnmr87w5r2j062snjj6g7cvj0thry7q0mp3w6",
-    outputs: [
-        {
-            address: "mmx16aq5vpcmxcrh9xck0z06eqnmr87w5r2j062snjj6g7cvj0thry7q0mp3w6",
-            contract: "mmx1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqdgytev",
-            amount: 1,
-            memo: "hello from js",
-        },
-    ],
-};
-
 import { Transaction } from "@/mmx/wallet/Transaction";
 import { JSONbigNative } from "@/mmx/wallet/utils/JSONbigNative";
-const contract = ref(JSONbigNative.stringify(init, null, 4));
+const contract = ref("{}");
 
 const getPayload = async () => {
-    const options = {
-        //fee_ratio: 2048,
-        expire_at: -1,
-        network: "mainnet",
-    };
-
     const tx = Transaction.parse(contract.value);
-
-    // tx.inputs = [];
-    // tx.solutions = [];
-    //await wallet.value.completeAsync(tx, options);
-
-    const payload = JSONbigNative.stringify(tx, null, 4);
-    contract.value = payload;
-
+    const payload = JSONbigNative.stringify(tx);
     return payload;
 };
 
 import { useTransactionValidate, useTransactionBroadcast } from "@/queries/wapi";
+
+const handleFormat = () => {
+    const obj = JSONbigNative.parse(contract.value);
+    contract.value = JSONbigNative.stringify(obj, null, 4);
+};
 
 const transactionValidate = useTransactionValidate();
 const handleValidate = async () => {
