af1b584c84984946baf8d7db888acb725b9da383
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-24 23:55:58 +0300

    refactor: streamline tab visibility logic in MenuTabs component

--- ui/src/components/MainMenu/components/MenuTabs.vue
+++ ui/src/components/MainMenu/components/MenuTabs.vue
@@ -1,31 +1,32 @@
 <template>
     <q-tabs indicator-color="primary" outside-arrows class="q-mx-xs">
         <template v-for="(item, key) in tabs" :key="key">
-            <template v-if="item.separator">
-                <q-separator vertical inset class="q-mx-md" />
-            </template>
-            <template v-else-if="item.space">
-                <q-space />
-            </template>
-            <template v-else-if="item.component">
-                <template v-if="item.tabTagWrap ?? true">
-                    <div class="q-tab self-stretch flex flex-center text-center">
+            <template v-if="item.visible ?? true">
+                <template v-if="item.separator">
+                    <q-separator vertical inset class="q-mx-md" />
+                </template>
+                <template v-else-if="item.space">
+                    <q-space />
+                </template>
+                <template v-else-if="item.component">
+                    <template v-if="item.tabTagWrap ?? true">
+                        <div class="q-tab self-stretch flex flex-center text-center">
+                            <component :is="item.component" />
+                        </div>
+                    </template>
+                    <template v-else>
                         <component :is="item.component" />
-                    </div>
+                    </template>
                 </template>
                 <template v-else>
-                    <component :is="item.component" />
+                    <q-route-tab
+                        :to="item.to"
+                        :label="item.label"
+                        :icon="item.icon"
+                        :disable="item.disable"
+                        @click="item.click"
+                    />
                 </template>
-            </template>
-            <template v-else>
-                <q-route-tab
-                    v-if="item.visible ?? true"
-                    :to="item.to"
-                    :label="item.label"
-                    :icon="item.icon"
-                    :disable="item.disable"
-                    @click="item.click"
-                />
             </template>
         </template>
     </q-tabs>
