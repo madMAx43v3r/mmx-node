0ce51eb9ed74669d0624fd734d63493a5594444f
Max Wittal <max.wittal@mwittal.de>
2026-08-08 14:47:58 +0700

    cuda cmake refactor

--- cmake/use_cuda.cmake
+++ cmake/use_cuda.cmake
@@ -1,13 +1,13 @@
 
-if(POLICY CMP0146)
-	# Keep using the legacy FindCUDA module until the CUDA build is migrated to first-class CUDA language support.
-	cmake_policy(SET CMP0146 OLD)
+include(CheckLanguage)
+
+check_language(CUDA)
+if(CMAKE_CUDA_COMPILER)
+	enable_language(CUDA)
+	set(MMX_CUDA_FOUND TRUE)
+
+	# Keep the toolkit location available for packaging the CUDA runtime on Windows.
+	get_filename_component(MMX_CUDA_TOOLKIT_BIN_DIR "${CMAKE_CUDA_COMPILER}" DIRECTORY)
+else()
+	set(MMX_CUDA_FOUND FALSE)
 endif()
-
-find_package(CUDA)
-
-if(NOT MSVC)
-	set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -Xcompiler ,-fPIC")
-endif()
-
-set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -diag-suppress 611,997,68,186 -gencode arch=compute_61,code=compute_61 -gencode arch=compute_75,code=compute_75 -gencode arch=compute_86,code=compute_86")
