1eda5420bb0358f139b154765621350a9266c0b4
voidxno <53347890+voidxno@users.noreply.github.com>
2026-08-30 10:43:34 +0200

    Optimization, TimeLord

--- src/sha256_ni_rec.cpp
+++ src/sha256_ni_rec.cpp
@@ -59,6 +59,10 @@ void recursive_sha256_ni(uint8_t* hash, const uint64_t num_iters)
 	const __m128i HPAD0_CACHE = _mm_set_epi64x(0x0000000000000000,0x0000000080000000);
 	const __m128i HPAD1_CACHE = _mm_set_epi64x(0x0000010000000000,0x0000000000000000);
 
+	// pre-calc/static values
+	const __m128i MORE0_CACHE = _mm_set_epi64x(0x5807AA985807AA98,0x550C7DC3243185BE);
+	const __m128i MORE1_CACHE = _mm_set_epi64x(0x0000000000000000,0x0000000000000000);
+
 	__m128i STATE0;
 	__m128i STATE1;
 	__m128i MSG;
@@ -102,7 +106,7 @@ void recursive_sha256_ni(uint8_t* hash, const uint64_t num_iters)
 		MSG = _mm_add_epi32(MSG,_mm_load_si128(reinterpret_cast<const __m128i*>(&K64[8])));
 		STATE1 = _mm_sha256rnds2_epu32(STATE1,STATE0,MSG);
 		MSG = _mm_shuffle_epi32(MSG,0x0E);
-		STATE0 = _mm_sha256rnds2_epu32(STATE0,STATE1,MSG);
+		STATE0 = _mm_sha256rnds2_epu32(STATE0,STATE1,MORE0_CACHE);
 		MSGTMP1 = _mm_sha256msg1_epu32(MSGTMP1,MSGTMP2);
 
 		// rounds 12-15
@@ -110,7 +114,7 @@ void recursive_sha256_ni(uint8_t* hash, const uint64_t num_iters)
 		MSGTMP3 = MSG;
 		MSG = _mm_add_epi32(MSG,_mm_load_si128(reinterpret_cast<const __m128i*>(&K64[12])));
 		STATE1 = _mm_sha256rnds2_epu32(STATE1,STATE0,MSG);
-		MSGTMP0 = _mm_add_epi32(MSGTMP0,_mm_alignr_epi8(MSGTMP3,MSGTMP2,4));
+		MSGTMP0 = _mm_add_epi32(MSGTMP0,MORE1_CACHE);
 		MSGTMP0 = _mm_sha256msg2_epu32(MSGTMP0,MSGTMP3);
 		MSG = _mm_shuffle_epi32(MSG,0x0E);
 		STATE0 = _mm_sha256rnds2_epu32(STATE0,STATE1,MSG);
